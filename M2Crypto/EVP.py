092c52ccba88077ce6baa04bfa206676f6b8a9a5
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
1999-09-12 14:28:31 +0000

    HMAC. Some PKEY.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@8 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/EVP.py
+++ M2Crypto/EVP.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python
+""" M2Crypto wrapper for OpenSSL EVP API.
+Copyright (c) 1999 Ng Pheng Siong. All rights reserved. """
 
-RCS_id='$Id: EVP.py,v 1.1 1999/08/16 15:31:48 ngps Exp $'
+RCS_id='$Id: EVP.py,v 1.2 1999/09/12 14:28:31 ngps Exp $'
 
 from M2Crypto import md_ctx_new, md_ctx_free, \
 	digest_init, digest_update, digest_final, \
 	md5, sha1, ripemd160, \
+	hmac_ctx_new, hmac_ctx_free, hmac_init, hmac_update, hmac_final, \
 	cipher_ctx_new, cipher_ctx_free, \
 	cipher_init, cipher_update, cipher_final, \
-	bytes_to_key, \
+	bytes_to_key, rc4, \
 	bf_ecb, bf_cbc, bf_cfb, bf_ofb,\
 	idea_ecb, idea_cbc, idea_cfb, idea_ofb,\
 	cast5_ecb, cast5_cbc, cast5_cfb, cast5_ofb,\
@@ -15,7 +17,7 @@ from M2Crypto import md_ctx_new, md_ctx_free, \
 	des_ecb, des_cbc, des_cfb, des_ofb,\
 	des_ede_ecb, des_ede_cbc, des_ede_cfb, des_ede_ofb,\
 	des_ede3_ecb, des_ede3_cbc, des_ede3_cfb, des_ede3_ofb,\
-	rc4
+	pkey_new, pkey_free, sign_init, sign_update, sign_final
 
 class MessageDigest:
 	def __init__(self, algo):
@@ -34,6 +36,28 @@ class MessageDigest:
 
 	def final(self):
 		return digest_final(self.ctx)
+	
+	digest=final
+
+class HMAC:
+	def __init__(self, algo, key):
+		self.md=eval(algo)()
+		if not self.md:
+			raise ValueError, 'unknown algorithm'
+		self.ctx=hmac_ctx_new()
+		hmac_init(self.ctx, key, self.md)
+
+	def __del__(self):
+		if self.ctx:
+			hmac_ctx_free(self.ctx)
+
+	def update(self, data):
+		hmac_update(self.ctx, data)
+
+	def final(self):
+		return hmac_final(self.ctx)
+	
+	digest=final
 
 class Cipher:
 	def __init__(self, alg, key, iv, op, key_as_bytes=0, d='md5', salt='', i=1):
@@ -58,4 +82,21 @@ class Cipher:
 
 	def final(self):
 		return cipher_final(self.ctx)
+
+class PKey:
+	def __init__(self, md, pkey=pkey_new()):
+		self.pkey=pkey
+		self.md=eval(md)()
+		if not self.md:
+			raise ValueError, 'unknown message digest'
+
+	def __del__(self):
+		if self.pkey:
+			pkey_free(self.pkey)
+
+	def update(self, data):
+		sign_update(self.ctx, data)
+
+	def final(self):
+		return sign_final(self.ctx, self.pkey)
 
