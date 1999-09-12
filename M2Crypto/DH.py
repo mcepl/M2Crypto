ba7a8859ac61bd4268ce70fca83e2e36f7632073
Ng Pheng Siong <ngps@netmemetic.com>
1999-09-12 14:26:32 +0000

    Clean up namespace. Use mpint as interface between Python and OpenSSL.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@6 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/DH.py
+++ M2Crypto/DH.py
@@ -1,31 +1,33 @@
-#!/usr/bin/env python
+""" M2Crypto wrapper for OpenSSL DH API.
+Copyright (c) 1999 Ng Pheng Siong. All rights reserved. """
 
-RCS_id='$Id: DH.py,v 1.1 1999/08/16 15:31:29 ngps Exp $'
+RCS_id='$Id: DH.py,v 1.2 1999/09/12 14:26:32 ngps Exp $'
 
-from _m2crypto import dh_init, dh_new, dh_free, dh_generate_parameters, \
-	dh_size, dh_get_g, dh_get_p, dh_get_pub, dh_get_priv, \
-	dh_set_p, dh_set_g, dh_check, dh_generate_key, dh_compute_key
-dh_init()
+from util import genparam_callback
+import M2Crypto
+m2=M2Crypto
+
+m2.dh_init()
 
 class DH:
-	def __init__(self, this=dh_new()):
+	def __init__(self, this=m2.dh_new()):
 		self.this=this
 
 	def __del__(self):
-		dh_free(self.this)
+		m2.dh_free(self.this)
 
 	def __len__(self):
-		return dh_size(self.this)
+		return m2.dh_size(self.this)
 
 	def __getattr__(self, name):
-		if name=='g':
-			return int(dh_get_g(self.this))
-		elif name=='p':
-			return long(dh_get_p(self.this))
+		if name=='p':
+			return m2.dh_get_p(self.this)
+		elif name=='g':
+			return m2.dh_get_g(self.this)
 		elif name=='pub':
-			return long(dh_get_pub(self.this))
+			return m2.dh_get_pub(self.this)
 		elif name=='priv':
-			return long(dh_get_priv(self.this))
+			return m2.dh_get_priv(self.this)
 		else:
 			raise AttributeError
 
@@ -38,31 +40,24 @@ class DH:
 			self.__dict__[name]=value
 
 	def set_params(self, p, g):
-		dh_set_p(self.this, str(p))	
-		dh_set_g(self.this, str(g))	
+		m2.dh_set_p(self.this, p)
+		m2.dh_set_g(self.this, g)
 
 	def check_params(self):
-		return dh_check(self.this)
+		return m2.dh_check(self.this)
 		
 	def gen_key(self):
-		dh_generate_key(self.this)	
+		m2.dh_generate_key(self.this)	
 
 	def compute_key(self, pubkey):
-		# XXX pubkey should be PyLong or BIGNUM, not PyString
-		return dh_compute_key(self.this, str(pubkey)[:-1])
-
-def genparam_callback(p, n):
-	from sys import stdout
-	ch=['.','+','*','\n']
-	stdout.write(ch[p])
-	stdout.flush()
+		return m2.dh_compute_key(self.this, pubkey)
 
 def gen_params(plen, g, callback=genparam_callback):
-	return DH(this=dh_generate_parameters(plen, g, callback))
+	return DH(this=m2.dh_generate_parameters(plen, g, callback))
 
 def load_params(file):
 	f=open(file)
-	dh=dh_read_params(f, callback)
+	dh=m2.dh_read_params(f, callback)
 	f.close()
 	return DH(dh)
 
