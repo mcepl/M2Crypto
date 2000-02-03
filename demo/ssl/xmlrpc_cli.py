01bcd4216e456c44e9d4bfcbf292dc2b327b8bca
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-02-03 16:37:50 +0000

    More test cases.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@27 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/ssl/xmlrpc_cli.py
+++ demo/ssl/xmlrpc_cli.py
@@ -2,13 +2,20 @@
 
 """Demonstration of M2Crypto.xmlrpclib2.
 
-Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
+Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: xmlrpc_cli.py,v 1.1 1999/10/19 15:23:19 ngps Exp $'
+RCS_id='$Id: xmlrpc_cli.py,v 1.2 2000/02/03 16:37:50 ngps Exp $'
 
 from M2Crypto.xmlrpclib2 import Server, SSL_Transport
 
+# XXX This works for Zope 2.0.x but not for Zope 2.1.x.
 # Server is Zope on ZServerSSL.
-zs=Server('https://127.0.0.1:8443/QuickStart', SSL_Transport())
-print zs.objectIds()
+#zs=Server('https://127.0.0.1:8443/QuickStart', SSL_Transport())
+#print zs.objectIds()
+
+# Server is ../https/START.py.
+zs=Server('https://127.0.0.1:9443/RPC2', SSL_Transport())
+print zs.Testing(1, 2, 3)
+print zs.BringOn('SOAP')
+
 
