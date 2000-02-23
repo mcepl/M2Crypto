e7e457f31822ec9560a1e4b98bcdf214e9347396
Ng Pheng Siong <ngps@netmemetic.com>
2000-02-23 15:43:09 +0000

    Code reorg - moved out SSL callbacks.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@30 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/util.py
+++ M2Crypto/util.py
@@ -2,7 +2,11 @@
 
 Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: util.py,v 1.3 2000/02/01 15:04:54 ngps Exp $'
+RCS_id='$Id: util.py,v 1.4 2000/02/23 15:43:09 ngps Exp $'
+
+import sys
+import M2Crypto
+m2 = M2Crypto
 
 def h2b(s):
     import array, string
@@ -15,9 +19,11 @@ def h2b(s):
         ar.append(chr(num))
     return ar.tostring()        
 
+
 def pkcs5_pad(data, blklen=8):
     pad=(8-(len(data)%8))
     return data+chr(pad)*pad
+
 
 def pkcs7_pad(data, blklen):
     if blklen>255:
@@ -25,17 +31,22 @@ def pkcs7_pad(data, blklen):
     pad=(blklen-(len(data)%blklen))
     return data+chr(pad)*pad
 
+
 def passphrase_callback(v, prompt1='Enter passphrase:', prompt2='Verify passphrase:'):
     from getpass import getpass
     while 1:
-        p1=getpass(prompt1)
-        if v:
-            p2=getpass(prompt2)
-            if p1==p2:
+        try:
+            p1=getpass(prompt1)
+            if v:
+                p2=getpass(prompt2)
+                if p1==p2:
+                    break
+            else:
                 break
-        else:
-            break
+        except KeyboardInterrupt:
+            return None
     return p1
+
 
 def genparam_callback(p, n):
     from sys import stdout
@@ -43,15 +54,6 @@ def genparam_callback(p, n):
     stdout.write(ch[p])
     stdout.flush()
 
-def ssl_verify_callback_stub(ok, x509_store_ctx_ptr):
-    return 1
-
-def ssl_verify_callback(ok, x509_store_ctx_ptr):
-    ctx=X509.X509_Store_Context(x509_store_ctx_ptr)
-    err_cert=m2.x509_store_ctx_get_current_cert(ctx)
-    err=m2.x509_store_ctx_get_error(ctx)
-    depth=m2.x509_store_ctx_get_current_cert(ctx)
-    # XXX boring stuff... 
 
 def octx_to_num(x):
     v = 0L
