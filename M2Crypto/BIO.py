2c55e555fdb8ef5b59a9472395e772588b800c06
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-04-01 14:49:38 +0000

    Removed unused incoherent junk.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@37 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/BIO.py
+++ M2Crypto/BIO.py
@@ -4,33 +4,30 @@ This is all a bit ad hoc and incoherent at the moment.
 
 Copyright (c) 1999, 2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: BIO.py,v 1.2 2000/01/31 16:23:25 ngps Exp $'
+RCS_id='$Id: BIO.py,v 1.3 2000/04/01 14:49:38 ngps Exp $'
 
 import M2Crypto
 m2=M2Crypto
 
 m2.bio_init()
 
-BIO_method={ \
-    's_mem': m2.bio_s_mem, \
-    's_socket': m2.bio_s_socket, \
-    'f_ssl': m2.bio_f_ssl, \
-    'f_buffer': m2.bio_f_buffer \
-    }
-
 class BIO:
-    def __init__(self, method=None):
-        if method is not None:
-            self.bio=m2.bio_new(BIO_method[kw_args['method']]())
-        self.closed=0
+    def __init__(self, bio, _pyfree=0):
+        self.bio = bio
+        self._pyfree = _pyfree
+        self.closed = 0
 
     def __del__(self):
         self.close()
-        m2.bio_free(self.bio)
+        if self._pyfree:
+            m2.bio_free(self.bio)
 
-    def bio_ptr(self):
+    def _ptr(self):
         # Friends only, please.
         return self.bio
+
+    # Deprecated.
+    bio_ptr = _ptr
 
     def readable(self):
         return 1
@@ -38,7 +35,7 @@ class BIO:
     def read(self, size=4096):
         if not self.readable():
             raise m2.Error("write-only") 
-        if self.closed or size<=0:
+        if self.closed or size <= 0:
             return ''
         try:
             return m2.bio_read(self.bio, size)
@@ -56,72 +53,21 @@ class BIO:
             return 0
         return m2.bio_write(self.bio, data)
 
+    def flush(self):
+        m2.bio_flush(self.bio)
+
     def close(self):
-        self.closed=1
-
-
-class IOBuffer(BIO):
-
-    """Class wrapper for BIO_f_buffer. Its principal function is to
-    be BIO_push()'ed on top of a BIO_s_socket, so that makefile() of
-    said socket works. Provides buffering for the underlying."""
-
-    def __init__(self, bio_ptr, mode):
-        self.io=m2.bio_new(m2.bio_f_buffer())
-        self.bio=m2.bio_push(self.io, bio_ptr)
-        self.closed=0
-        if 'w' in mode:
-            self.can_write=1
-        else:
-            self.can_write=0
-
-    def __del__(self):
-        m2.bio_pop(self.bio)
-        m2.bio_free(self.io)
-
-    def fileno(self):
-        # XXX Caller is not expected to expect to do anything useful with this.
-        return id(self)
-
-    def readline(self, size=80):
-        if self.closed:
-            return None
-        buf=m2.bio_gets(self.bio, size)
-        if buf is None:
-            return ''
-        return buf
-
-    def readlines(self, sizehint='ignored'):
-        if self.closed:
-            return []
-        lines=[]
-        while 1:
-            buf=m2.bio_gets(self.bio, 80)
-            if buf is None:
-                break
-            lines.append(buf)
-        return lines
-
-    def writeable(self):
-        return self.can_write
-
-
-class Socket(BIO):
-
-    """Class wrapper for BIO_s_socket."""
-
-    def __init__(self, sock_fileno, close_flag=0):
-        BIO.__init__(self)
-        self.bio=m2.bio_new_socket(sock_fileno, close_flag)
+        self.closed = 1
 
 
 class MemoryBuffer(BIO):
 
-    """Class wrapper for BIO_s_mem."""
+    """Object wrapper for BIO_s_mem."""
 
     def __init__(self, data=None):
-        BIO.__init__(self)
-        self.bio=m2.bio_new(m2.bio_s_mem())
+        BIO.__init__(self, None)
+        self.bio = m2.bio_new(m2.bio_s_mem())
+        self._pyfree = 1
         if data is not None:
             m2.bio_write(self.bio, data)
 
@@ -134,19 +80,22 @@ class MemoryBuffer(BIO):
         except:
             return ''
         
-    getvalue=read_all
+    # StringIO-compatibility.
+    getvalue = read_all
+
 
 class File(BIO):
 
-    """Class wrapper for BIO_s_fp. This class is intended to interface Python
+    """Object wrapper for BIO_s_fp. This class is intended to interface Python
     and OpenSSL functions that expect BIO *. If you wish to manipulate files 
     in Python, use Python's file object."""
 
     def __init__(self, pyfile, close_flag=0):
-        BIO.__init__(self)
-        self.pyfile=pyfile
-        self.close_flag=close_flag
+        BIO.__init__(self, None)
+        self.pyfile = pyfile
+        self.close_flag = close_flag
         self.bio=m2.bio_new_fp(pyfile, 0)
+        self._pyfree = 1
 
     def __del__(self):
         m2.bio_free(self.bio)
