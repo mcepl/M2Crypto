b4124454390d7791a127925c108384c4a2f24a94
Ng Pheng Siong <ngps@netmemetic.com>
2000-04-01 14:55:32 +0000

    Fixed send()/recv() to use the unified ssl_write()/ssl_read().
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@41 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/SSL/ssl_dispatcher.py
+++ M2Crypto/SSL/ssl_dispatcher.py
@@ -1,9 +1,9 @@
 """Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: ssl_dispatcher.py,v 1.1 2000/02/23 15:39:31 ngps Exp $'
+RCS_id='$Id: ssl_dispatcher.py,v 1.2 2000/04/01 14:55:32 ngps Exp $'
 
 # Python
-import asyncore, socket, sys
+import asyncore, socket
 
 # M2Crypto
 from Connection import Connection
@@ -20,32 +20,9 @@ class ssl_dispatcher(asyncore.dispatcher):
         self.socket.setblocking(0)
         self.add_channel()
 
-    def send(self, data):
-        (n, err)=m2.ssl_write_nbio(self.ssl, data)
-        if n==-1:
-            if err==m2.ssl_error_zero_return:
-                return 0
-            elif err==m2.ssl_error_syscall:
-                raise Err.SSLError(n, self.socket.getpeername())
-            else:       # Only in non-blocking mode. 
-                return -1
-        else:
-            if n==0:    # In non-blocking SSL, this means 'try again'.
-                return 0
-            else:
-                return n
-
     def recv(self, size=4096):
-        (n, blob, err)=m2.ssl_read_nbio(self.ssl, size)
-        if n==-1:
-            if err==m2.ssl_error_zero_return:
-                return ''
-            elif err in [m2.ssl_error_ssl, m2.ssl_error_syscall]:
-                m2.err_print_errors_fp(sys.stderr)
-                raise Err.SSLError(n, self.socket.getpeername())
-            else:
-                return None # XXX None is overloaded to mean 'try-again'.
-        else:
-            return blob
+        return self.socket.recv(size)
 
+    def send(self, buffer):
+        return self.socket.send(buffer)
 
