3706f120c1d90ab905ce8177c3459b751995ce58
Ng Pheng Siong <ngps@netmemetic.com>
2000-04-01 14:54:18 +0000

    Now handles basic authentication over https.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@40 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/m2xmlrpclib.py
+++ M2Crypto/m2xmlrpclib.py
@@ -2,10 +2,12 @@
 
 Copyright (c) 1999 Ng Pheng Siong. All rights reserved. """
 
-RCS_id='$Id: m2xmlrpclib.py,v 1.1 1999/10/18 15:07:06 ngps Exp $'
+RCS_id='$Id: m2xmlrpclib.py,v 1.2 2000/04/01 14:54:18 ngps Exp $'
+
+import base64, string
 
 from xmlrpclib import *
-import httpslib, SSL
+import SSL, httpslib, urllib2
 
 __version__='0.03'
 
@@ -13,34 +15,34 @@ class SSL_Transport(Transport):
 
     user_agent = "xmlrpc_ssl.py/%s - %s" % (__version__, Transport.user_agent)
 
-    def __init__(self, ssl_context=None, proxy_host=None, proxy_port=8080):
-        self.use_proxy=0
-        if proxy_host is not None:
-            self.proxy_host=proxy_host
-            self.proxy_port=proxy_port
-            self.use_proxy=1
+    def __init__(self, ssl_context=None):
         if ssl_context is None:
             self.ssl_ctx=SSL.Context('sslv23')
         else:
             self.ssl_ctx=ssl_context
 
     def request(self, host, handler, request_body):
-        if self.use_proxy:
-            h = httpslib.HTTPS(self.ssl_ctx, self.proxy_host, self.proxy_port)
-        else:
-            h = httpslib.HTTPS(self.ssl_ctx, host)
-        req = 'https://%s%s' % (host, handler)
+        # Handle username and password.
+        user_passwd, host_port = urllib2.splituser(host)
+        _host, _port = urllib2.splitport(host_port)
+        h = httpslib.HTTPS(self.ssl_ctx, _host, int(_port))
+        h.set_debuglevel(1)
 
-        # Everything that follows is as in xmlrpclib.Transport.
+        # What follows is as in xmlrpclib.Transport. (Except the authz bit.)
         h.putrequest("POST", handler)
 
         # required by HTTP/1.1
-        h.putheader("Host", host)
+        h.putheader("Host", _host)
 
         # required by XML-RPC
         h.putheader("User-Agent", self.user_agent)
         h.putheader("Content-Type", "text/xml")
         h.putheader("Content-Length", str(len(request_body)))
+
+        # Authorisation.
+        if user_passwd is not None:
+            auth=string.strip(base64.encodestring(user_passwd))
+            h.putheader('Authorization', 'Basic %s' % auth)
 
         h.endheaders()
 
