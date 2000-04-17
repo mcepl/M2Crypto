436f9ad0026d60a68b0fa6c6dbd31dd4558398e0
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-04-17 16:13:11 +0000

    Patched class IOBuffer back in.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@56 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/BIO.py
+++ M2Crypto/BIO.py
@@ -4,7 +4,7 @@ This is all a bit ad hoc and incoherent at the moment.
 
 Copyright (c) 1999, 2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: BIO.py,v 1.3 2000/04/01 14:49:38 ngps Exp $'
+RCS_id='$Id: BIO.py,v 1.4 2000/04/17 16:13:11 ngps Exp $'
 
 import M2Crypto
 m2=M2Crypto
@@ -53,6 +53,9 @@ class BIO:
             return 0
         return m2.bio_write(self.bio, data)
 
+    def reset(self):
+        m2.bio_reset(self.bio)
+
     def flush(self):
         m2.bio_flush(self.bio)
 
@@ -74,21 +77,24 @@ class MemoryBuffer(BIO):
     def __len__(self):
         return m2.bio_ctrl_pending(self.bio)
 
-    def read_all(self):
-        try:
+    def read(self, size=0):
+        if size:
+            return m2.bio_read(self.bio, size)
+        else:
             return m2.bio_read(self.bio, m2.bio_ctrl_pending(self.bio))
-        except:
-            return ''
+            
+    #def read_all(self):
+    #    return m2.bio_read(self.bio, m2.bio_ctrl_pending(self.bio))
         
-    # StringIO-compatibility.
-    getvalue = read_all
+    # Backwards-compatibility.
+    getvalue = read_all = read
 
 
 class File(BIO):
 
     """Object wrapper for BIO_s_fp. This class is intended to interface Python
-    and OpenSSL functions that expect BIO *. If you wish to manipulate files 
-    in Python, use Python's file object."""
+    and OpenSSL functions that expect BIO *. For general file manipulation in
+    Python, use Python's file object."""
 
     def __init__(self, pyfile, close_flag=0):
         BIO.__init__(self, None)
@@ -104,4 +110,51 @@ class File(BIO):
 def openfile(filename, mode='rb'):
     return File(open(filename, mode), 1)
 
+
+class IOBuffer(BIO):
+
+    """Object wrapper for BIO_f_buffer. Its principal function is to
+    be BIO_push()'ed on top of a BIO_f_ssl, so that makefile() of
+    said underlying SSL socket works."""
+
+    def __init__(self, bio_ptr, mode, _pyfree=1):
+        self.io = m2.bio_new(m2.bio_f_buffer())
+        self.bio = m2.bio_push(self.io, bio_ptr)
+        self.closed = 0
+        if 'w' in mode:
+            self.can_write=1
+        else:
+            self.can_write=0
+        self._pyfree = _pyfree
+
+    def __del__(self):
+        if self._pyfree:
+            m2.bio_pop(self.bio)
+            m2.bio_free(self.io)
+
+    def fileno(self):
+        # XXX Caller is not expected to expect to do anything useful with this.
+        return id(self)
+
+    def readline(self, size=80):
+        if self.closed:
+            return None
+        buf = m2.bio_gets(self.bio, size)
+        if buf is None:
+            return ''
+        return buf
+
+    def readlines(self, sizehint='ignored'):
+        if self.closed:
+            return []
+        lines=[]
+        while 1:
+            buf=m2.bio_gets(self.bio, 80)
+            if buf is None:
+                break
+            lines.append(buf)
+        return lines
+
+    def writeable(self):
+        return self.can_write
 
