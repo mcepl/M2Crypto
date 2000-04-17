ad5516dfdebc69eca5d48d8add197bf1ad2324d4
Ng Pheng Siong <ngps@netmemetic.com>
2000-04-17 16:16:44 +0000

    OpenSSL 0.9.5 compatible.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@57 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/Rand.py
+++ M2Crypto/Rand.py
@@ -1,6 +1,20 @@
-#!/usr/bin/env python
+"""M2Crypto wrapper for OpenSSL PRNG. Requires OpenSSL 0.9.5 and above.
 
-RCS_id='$Id: Rand.py,v 1.1 1999/08/16 15:32:52 ngps Exp $'
+Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-from M2Crypto import rand_bytes, rand_cleanup
+RCS_id='$Id: Rand.py,v 1.2 2000/04/17 16:16:44 ngps Exp $'
+
+import M2Crypto
+m2 = M2Crypto
+
+rand_seed           = m2.rand_seed
+rand_add            = m2.rand_add
+load_file           = m2.rand_load_file
+save_file           = m2.rand_save_file
+rand_bytes          = m2.rand_bytes
+rand_pseudo_bytes   = m2.rand_pseudo_bytes
+
+def estimate_entropy(buf):
+    pass
+
 
