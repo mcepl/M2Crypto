d57b49a744c86c50c923d2cf7bf668d8bf29775b
Ng Pheng Siong <ngps@netmemetic.com>
2000-04-17 15:58:15 +0000

    Various enhancements.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@53 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/ssl/echod_lib.py
+++ demo/ssl/echod_lib.py
@@ -2,42 +2,48 @@
 
 """Support routines for the various SSL 'echo' servers.
 
-Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
+Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: echod_lib.py,v 1.1 1999/10/01 16:13:24 ngps Exp $'
+RCS_id='$Id: echod_lib.py,v 1.2 2000/04/17 15:58:15 ngps Exp $'
 
 import SocketServer
-from M2Crypto import SSL
+from M2Crypto import Err, SSL
 
 def init_context(protocol, certfile, cafile, verify, verify_depth=10):
     ctx=SSL.Context(protocol)
     ctx.load_cert(certfile)
-    ctx.load_verify_location(cafile)
-    ctx.load_client_CA(cafile)
+    ctx.load_client_ca(cafile)
+    ctx.load_verify_info(cafile)
     ctx.set_verify(verify, verify_depth)
+    ctx.set_allow_unknown_ca(1)
+    ctx.set_session_id_ctx('echod')
+    ctx.set_info_callback()
     return ctx
+
 
 class ssl_echo_handler(SocketServer.BaseRequestHandler):
 
-    buffer='Ye Olde Echo Servre'
+    buffer='Ye Olde Echo Servre\r\n'
 
     def handle(self):
-        self.get_peer()
+        if not self.request.verify_ok():
+            v = self.request.get_verify_result()
+            print 'peer verification failed:', Err.get_x509_verify_error(v)
+            return
+        peer = self.request.get_peer_cert()
+        if peer is not None:
+            print 'Client CA        =', peer.get_issuer().O
+            print 'Client Subject   =', peer.get_subject()
         self.request.write(self.buffer)
         while 1:
-            buf=self.request.read()
-            if not buf:
+            try:
+                buf = self.request.read()
+                if not buf:
+                    break
+                self.request.write(buf) 
+            except:
                 break
-            self.request.write(buf) 
 
     def finish(self):
         self.request.close()
-
-    def get_peer(self):
-        while 1:
-            peer=self.request.get_peer_cert()
-            if peer is not None:
-                print peer.as_text()
-                break
-
 
