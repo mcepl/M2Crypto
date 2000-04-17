f02000d2a9a19056c482dee765e3aab55a770a81
Ng Pheng Siong <ngps@netmemetic.com>
2000-04-17 16:18:40 +0000

    EVP, PGP.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@60 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/__init__.py
+++ M2Crypto/__init__.py
@@ -1,19 +1,19 @@
 """ M2Crypto
 Copyright (c) 1999 Ng Pheng Siong. All rights reserved. """
 
-RCS_id='$Id: __init__.py,v 1.2 1999/09/12 14:32:21 ngps Exp $'
-
-import sys
+RCS_id='$Id: __init__.py,v 1.3 2000/04/17 16:18:40 ngps Exp $'
 
 import _m2crypto
 import BIO
 import Rand
 import DH
 import DSA
+import EVP
 import RSA
 import RC4
 import SSL
 import X509
+import PGP
 
 encrypt=1
 decrypt=0
