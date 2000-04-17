3a4a3806a0bbef8fba9a0511cd1f742952b99554
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-04-17 15:36:29 +0000

    Fix bugs in test functions so they now work.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@48 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/smime/test.py
+++ demo/smime/test.py
@@ -4,11 +4,22 @@
 
 Copyright (c) 2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: test.py,v 1.1 2000/04/01 15:08:30 ngps Exp $'
+RCS_id='$Id: test.py,v 1.2 2000/04/17 15:36:29 ngps Exp $'
 
-from M2Crypto import BIO, SMIME, X509
+from M2Crypto import BIO, Rand, SMIME, X509
 
-ptxt = 'pgp -vs- smime'
+ptxt = """
+S/MIME - Secure Multipurpose Internet Mail Extensions [RFC 2311, RFC 2312] - 
+provides a consistent way to send and receive secure MIME data. Based on the
+popular Internet MIME standard, S/MIME provides the following cryptographic
+security services for electronic messaging applications - authentication,
+message integrity and non-repudiation of origin (using digital signatures)
+and privacy and data security (using encryption).
+
+S/MIME is built on the PKCS #7 standard. [PKCS7]
+
+S/MIME is implemented in Netscape Messenger and Microsoft Outlook.
+"""
 
 def makebuf():
     buf = BIO.MemoryBuffer(ptxt)
@@ -20,7 +31,7 @@ def sign():
     s = SMIME.SMIME()
     s.load_key('client.pem')
     p7 = s.sign(buf)
-    out = BIO.openfile('p7.clear', 'w')
+    out = BIO.openfile('clear.p7', 'w')
     out.write('To: ngps@post1.com\n')
     out.write('From: m2crypto@m2crypto.org\n')
     out.write('Subject: testing\n')
@@ -30,7 +41,7 @@ def sign():
 
     buf = makebuf()
     p7 = s.sign(buf)
-    out = BIO.openfile('p7.opaque', 'w')
+    out = BIO.openfile('opaque.p7', 'w')
     out.write('To: ngps@post1.com\n')
     out.write('From: m2crypto@m2crypto.org\n')
     out.write('Subject: testing\n')
@@ -48,9 +59,9 @@ def verify_clear():
     st = X509.X509_Store()
     st.load_info('ca.pem')
     s.set_x509_store(st)
-    p7, data = SMIME.load_pkcs7('p7.clear')
+    p7, data = SMIME.load_pkcs7('clear.p7')
     v = s.verify(p7)
-    if v == ptxt:
+    if v:
         print 'ok'
     else:
         print 'not ok'
@@ -65,9 +76,9 @@ def verify_opaque():
     st = X509.X509_Store()
     st.load_info('ca.pem')
     s.set_x509_store(st)
-    p7, data = SMIME.load_pkcs7('p7.opaque')
+    p7, data = SMIME.load_pkcs7('opaque.p7')
     v = s.verify(p7, data)
-    if v == ptxt:
+    if v:
         print 'ok'
     else:
         print 'not ok'
@@ -95,24 +106,27 @@ def sv():
     # Load a private key.
     s.load_key('client.pem')
 
-    # Load signer cert(s).
+    # Sign.
+    p7 = s.sign(buf)
+
+    # Output the stuff.
+    bio = BIO.MemoryBuffer()
+    s.write(bio, p7, buf)
+    
+    # Plumbing for verification: CA's cert.
+    st = X509.X509_Store()
+    st.load_info('ca.pem')
+    s.set_x509_store(st)
+
+    # Plumbing for verification: Signer's cert.
     x509 = X509.load_cert('client.pem')
     sk = X509.X509_Stack()
     sk.push(x509)
     s.set_x509_stack(sk)
 
-    # Sign.
-    p7 = s.sign(buf, SMIME.PKCS7_DETACHED)
-    #p7 = s.sign(buf)
-    
-    # Construct verification plumbing.
-    st = X509.X509_Store()
-    st.load_info('ca.pem')
-    s.set_x509_store(st)
-
     # Verify.
-    buf = makebuf()
-    v = s.verify(p7, buf, SMIME.PKCS7_DETACHED)
+    p7, buf = SMIME.load_pkcs7_bio(bio)
+    v = s.verify(p7, flags=SMIME.PKCS7_DETACHED)
     
     if v:
         print 'ok'
@@ -142,17 +156,19 @@ def ed():
     # Decrypt.
     data = s.decrypt(p7)
     
-    if data == ptxt:
+    if data:
         print 'ok'
     else:
         print 'not ok'
 
 
 if __name__ == '__main__':
+    Rand.load_file('../randpool.dat', -1) 
     ed()
-    #sv()
     sign()
     verify_opaque()
     verify_clear()
     verify_netscape()
+    sv()
+    Rand.save_file('../randpool.dat')
 
