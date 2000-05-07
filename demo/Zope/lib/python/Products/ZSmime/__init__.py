d04f6270405cf28ed20a06d1eacd527a716cbcc4
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-05-07 15:59:27 +0000

    Initial revision
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@63 2715db39-9adf-0310-9c64-84f055769b4b

---
+++ demo/Zope/lib/python/Products/ZSmime/__init__.py
@@ -0,0 +1,10 @@
+"""ZSmime.__init__
+
+Copyright (c) 2000 Ng Pheng Siong. All rights reserved.
+This software is released under the ZPL. Usual disclaimers apply."""
+
+RCS_id = '$Id: __init__.py,v 1.1 2000/05/07 15:59:27 ngps Exp $'
+__version__='$Revision: 1.1 $'[11:-2]
+
+import SmimeTag
+
