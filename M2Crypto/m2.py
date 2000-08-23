7f251327709e1556f962fe29c3e949e63ecc81a7
Ng Pheng Siong <ngps@netmemetic.com>
2000-08-23 15:39:37 +0000

    Initial revision
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@70 2715db39-9adf-0310-9c64-84f055769b4b

---
+++ M2Crypto/m2.py
@@ -0,0 +1,10 @@
+"""M2Crypto
+
+Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
+
+RCS_id='$Id: m2.py,v 1.1 2000/08/23 15:30:47 ngps Exp $'
+
+from _m2crypto import *
+lib_init()
+
+
---
+++ demo/ssl/sess.py
@@ -0,0 +1,85 @@
+"""M2Crypto.SSL.Session client demo: This program requests a URL from 
+a HTTPS server, saves the negotiated SSL session id, parses the HTML 
+returned by the server, then requests each HREF in a separate thread 
+using the saved SSL session id.
+
+Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
+
+RCS_id='$Id: sess.py,v 1.1 2000/08/23 15:39:37 ngps Exp $'
+
+from M2Crypto import Err, Rand, SSL, X509, threading
+m2_threading = threading; del threading
+
+import formatter, getopt, htmllib, sys
+from threading import Thread
+from socket import gethostname
+
+
+def handler(sslctx, host, port, href, recurs=0, sslsess=None):
+
+    s = SSL.Connection(sslctx)
+    if sslsess:
+        s.set_session(sslsess)
+        s.connect((host, port))
+    else:
+        s.connect((host, port))
+        sslsess = s.get_session()
+
+    if recurs:
+        p = htmllib.HTMLParser(formatter.NullFormatter())
+
+    s.send(href)
+    while 1:
+        data = s.recv()
+        if not data:
+            break
+        if recurs:
+            p.feed(data)
+
+    if recurs:
+        p.close()
+
+    s.close()
+
+    if recurs:
+        for a in p.anchorlist:
+            req = 'GET %s HTTP/1.0\r\n\r\n' % a
+            thr = Thread(target=handler, 
+                        args=(sslctx, host, port, req, recurs-1, sslsess))
+            thr.start()
+    
+
+if __name__ == '__main__':
+
+    m2_threading.init()
+    Rand.load_file('../randpool.dat', -1) 
+
+    host = '127.0.0.1'
+    port = 9443
+    req = '/'
+
+    optlist, optarg = getopt.getopt(sys.argv[1:], 'h:p:r:')
+    for opt in optlist:
+        if '-h' in opt:
+            host = opt[1]
+        elif '-p' in opt:
+            port = int(opt[1])
+        elif '-r' in opt:
+            req = opt[1]
+    
+    ctx = SSL.Context('sslv3')
+    ctx.load_cert('client.pem')
+    ctx.load_verify_info('ca.pem')
+    ctx.load_client_ca('ca.pem')
+    ctx.set_verify(SSL.verify_none, 10)
+    
+    req = 'GET %s HTTP/1.0\r\n\r\n' % req
+
+    start = Thread(target=handler, args=(ctx, host, port, req, 1))
+    start.start()
+    start.join()
+    
+    m2_threading.cleanup()
+    Rand.save_file('../randpool.dat')
+
+
