3a9684d5f3c9edf6d49ae3265efe1c497babeead
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-08-23 15:36:29 +0000

    Cleaned up the code somewhat.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@69 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/SSL/cb.py
+++ M2Crypto/SSL/cb.py
@@ -1,14 +1,13 @@
 """Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: cb.py,v 1.2 2000/04/01 14:59:52 ngps Exp $'
+RCS_id='$Id: cb.py,v 1.3 2000/08/23 15:36:29 ngps Exp $'
 
 # Python
 import sys
 
 # M2Crypto
-import Context
-from M2Crypto import X509, M2Crypto
-m2 = M2Crypto
+import Connection, Context
+from M2Crypto import X509, m2
 
 def ssl_verify_callback_stub(ssl_ctx_ptr, x509_ptr, errnum, errdepth, ok):
     return ok
@@ -23,7 +22,7 @@ def ssl_verify_callback(ssl_ctx_ptr, x509_ptr, errnum, errdepth, ok):
     ssl_ctx = Context.map()[ssl_ctx_ptr]
     if errnum in unknown_issuer: 
         if ssl_ctx.get_allow_unknown_ca():
-            sys.stderr.write("%s: allow anyway...\n" % (m2.x509_get_verify_error(errnum)))
+            sys.stderr.write("policy: %s: permitted...\n" % (m2.x509_get_verify_error(errnum)))
             sys.stderr.flush()
             ok = 1
     # CRL checking goes here...
@@ -36,7 +35,18 @@ def ssl_verify_callback(ssl_ctx_ptr, x509_ptr, errnum, errdepth, ok):
 
 
 # Cribbed from OpenSSL's apps/s_cb.c.
+def ssl_info_callback0(where, ret, ssl_ptr):
+    ssl_conn = Connection.map()[ssl_ptr]
+    sys.stdout.write(ssl_ptr + ':' + str(sys.getrefcount(ssl_conn)) + '\n')
+    sys.stdout.flush()
+
+
 def ssl_info_callback(where, ret, ssl_ptr):
+
+    #ssl_conn = Connection.map()[ssl_ptr]
+    #sys.stdout.write(ssl_ptr + ':' + str(sys.getrefcount(ssl_conn)) + '\n')
+    #sys.stdout.flush()
+
     w = where & ~m2.SSL_ST_MASK
     if (w & m2.SSL_ST_CONNECT):
         state = "SSL connect"
@@ -48,26 +58,34 @@ def ssl_info_callback(where, ret, ssl_ptr):
     if (where & m2.SSL_CB_LOOP):
         sys.stderr.write("LOOP: %s: %s\n" % (state, m2.ssl_get_state_v(ssl_ptr)))
         sys.stderr.flush()
+        return
 
-    elif (where & m2.SSL_CB_ALERT):
-        if (where & m2.SSL_CB_READ):
-            w = 'read'
-        else:
-            w = 'write'
-        sys.stderr.write("ALERT: %s: %s: %s\n" % \
-            (w, m2.ssl_get_alert_type_v(ret), m2.ssl_get_alert_desc_v(ret)))
-        sys.stderr.flush()
-
-    elif (where & m2.SSL_CB_EXIT):
+    if (where & m2.SSL_CB_EXIT):
         if not ret:
             sys.stderr.write("FAILED: %s: %s\n" % (state, m2.ssl_get_state_v(ssl_ptr)))
             sys.stderr.flush()
         else:
             sys.stderr.write("INFO: %s: %s\n" % (state, m2.ssl_get_state_v(ssl_ptr)))
             sys.stderr.flush()
+        return
 
-    elif (where & m2.SSL_CB_CONNECT_EXIT):
-        sys.stderr.write("CONNECT_EXIT: %d\n" % (ret))
+    if (where & m2.SSL_CB_ALERT):
+        #ssl_conn = Connection.map()[ssl_ptr]
+        if (where & m2.SSL_CB_READ):
+            w = 'read'
+        #   ssl_attr = ssl_conn._read_closed
+        else:
+            w = 'write'
+        #   ssl_attr = ssl_conn._write_closed
+        sys.stderr.write("ALERT: %s: %s: %s\n" % \
+            (w, m2.ssl_get_alert_type_v(ret), m2.ssl_get_alert_desc_v(ret)))
         sys.stderr.flush()
- 
+        #if m2.ssl_get_alert_desc(ret) == SSL_AD_CLOSE_NOTIFY:
+        #    ssl_attr = 1
+        #if ssl_conn._read_closed and ssl_conn._write_closed:
+        #    sys.stderr.write("Deleting " + ssl_ptr + '\n')
+        #    sys.stderr.flush()
+        #    del Connection.map()[ssl_ptr]
+        return
+
 
