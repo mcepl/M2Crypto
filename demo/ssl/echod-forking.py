9e4a51281daf387ab56cf78713bde010468da340
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-08-23 15:46:21 +0000

    M2Crypto.threading.init() added.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@72 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/ssl/echod-forking.py
+++ demo/ssl/echod-forking.py
@@ -4,9 +4,9 @@
 
 Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: echod-forking.py,v 1.3 2000/04/17 15:55:52 ngps Exp $'
+RCS_id='$Id: echod-forking.py,v 1.4 2000/08/23 15:45:41 ngps Exp $'
 
-from M2Crypto import DH, Rand, SSL
+from M2Crypto import DH, Rand, SSL, threading
 import echod_lib
 
 class ssl_echo_handler(echod_lib.ssl_echo_handler):
@@ -15,9 +15,11 @@ class ssl_echo_handler(echod_lib.ssl_echo_handler):
 
 if __name__=='__main__':
     Rand.load_file('../randpool.dat', -1) 
+    threading.init()
     ctx=echod_lib.init_context('sslv23', 'server.pem', 'ca.pem', SSL.verify_peer)
     ctx.set_tmp_dh('dh1024.pem')
     s=SSL.ForkingSSLServer(('', 9999), ssl_echo_handler, ctx)
     s.serve_forever()   
+    threading.cleanup()
     Rand.save_file('../randpool.dat')
 
--- demo/ssl/echod-iterative.py
+++ demo/ssl/echod-iterative.py
@@ -4,9 +4,9 @@
 
 Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: echod-iterative.py,v 1.3 2000/04/17 15:56:53 ngps Exp $'
+RCS_id='$Id: echod-iterative.py,v 1.4 2000/08/23 15:46:21 ngps Exp $'
 
-from M2Crypto import Rand, SSL
+from M2Crypto import Rand, SSL, threading
 import echod_lib
 
 class ssl_echo_handler(echod_lib.ssl_echo_handler):
@@ -15,10 +15,13 @@ class ssl_echo_handler(echod_lib.ssl_echo_handler):
 
 if __name__=='__main__':
     Rand.load_file('../randpool.dat', -1) 
+    threading.init()
     ctx=echod_lib.init_context('sslv23', 'server.pem', 'ca.pem', \
-        SSL.verify_peer | SSL.verify_fail_if_no_peer_cert)
+        #SSL.verify_peer | SSL.verify_fail_if_no_peer_cert)
+        SSL.verify_none)
     ctx.set_tmp_dh('dh1024.pem')
     s=SSL.SSLServer(('', 9999), ssl_echo_handler, ctx)
     s.serve_forever()   
+    threading.cleanup()
     Rand.save_file('../randpool.dat')
 
