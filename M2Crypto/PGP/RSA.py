c3b4d46a1bdb54cb2155598a8e0e8699447557da
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-11-26 09:49:47 +0000

    Modified to conform to M2Crypto.RSA's new interface.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@82 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/PGP/RSA.py
+++ M2Crypto/PGP/RSA.py
@@ -1,29 +1,44 @@
 """M2Crypto PGP2 RSA.
 
-Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
+Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: RSA.py,v 1.1 1999/12/22 15:51:53 ngps Exp $'
+RCS_id='$Id: RSA.py,v 1.2 2000/11/26 09:49:47 ngps Exp $'
 
-from M2Crypto import RSA, M2Crypto
-m2=M2Crypto
+import sys
+from M2Crypto import m2
 
-class _RSA(RSA.RSA):
+if sys.version[:3] == '2.0':
+    from M2Crypto import RSA as _RSA
+
+elif sys.version[:3] == '1.5':
+    from M2Crypto import RSA
+    _RSA = RSA
+    del RSA
+
+else:
+    raise RuntimeError, 'unknown Python version'
+
+
+class RSA(_RSA.RSA):
     pass
 
-class _RSA_pub(RSA.RSA_pub):
-    def __setattr__(self, name, value):
-        # XXX Raise exception if e or n are not in order.
-        if name=='e':
-            return m2.rsa_set_e_bin(self.this, value)
-        elif name=='n':
-            return m2.rsa_set_n_bin(self.this, value)
-        else:
-            self.__dict__[name]=value
-        
-def new_pub_key(e, n):
-    cptr=m2.rsa_new()
-    r=_RSA_pub(cptr)
-    r.e=e
-    r.n=n
-    return r
+
+class RSA_pub(_RSA.RSA_pub):
+    pass
+
+
+def new_pub_key((e, n)):
+    """
+    Factory function that instantiates an RSA_pub object from a (e, n) tuple.
+
+    'e' is the RSA public exponent; it is a string in OpenSSL's binary format,
+    i.e., a number of bytes in big-endian.
+
+    'n' is the RSA composite of primes; it is a string in OpenSSL's binary format,
+    i.e., a number of bytes in big-endian.
+    """ 
+    rsa = m2.rsa_new()
+    m2.rsa_set_e_bin(rsa, e)
+    m2.rsa_set_n_bin(rsa, n)
+    return RSA_pub(rsa, 1)
 
