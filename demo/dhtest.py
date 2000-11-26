69c91549bdeb2e7387fc6fea883af1778328414d
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-11-26 08:27:24 +0000

    Modified for small interface change.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@80 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/dhtest.py
+++ demo/dhtest.py
@@ -4,25 +4,26 @@
 
 Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: dhtest.py,v 1.1 1999/09/12 14:44:08 ngps Exp $'
+RCS_id='$Id: dhtest.py,v 1.2 2000/11/26 08:27:24 ngps Exp $'
 
-from M2Crypto import DH
+from M2Crypto import DH, Rand
 
 def test():
-	print 'generating dh params:'
-	a=DH.gen_params(128,2)
-	b=DH.DH()
-	b.set_params(a.p, a.g)
-	a.gen_key()
-	b.gen_key()
-	print 'p = ', `a.p`
-	print 'g = ', `a.g`
-	print 'a.pub =', `a.pub`
-	print 'a.priv =', `a.priv`
-	print 'b.pub =', `b.pub`
-	print 'b.priv =', `b.priv`
-	print 'a.key = ', `a.compute_key(b.pub)`
-	print 'b.key = ', `b.compute_key(a.pub)`
+    print 'generating dh params:'
+    a = DH.gen_params(128, 2)
+    b = DH.set_params(a.p, a.g)
+    a.gen_key()
+    b.gen_key()
+    print 'p = ', `a.p`
+    print 'g = ', `a.g`
+    print 'a.pub =', `a.pub`
+    print 'a.priv =', `a.priv`
+    print 'b.pub =', `b.pub`
+    print 'b.priv =', `b.priv`
+    print 'a.key = ', `a.compute_key(b.pub)`
+    print 'b.key = ', `b.compute_key(a.pub)`
 
 if __name__=='__main__':
-	test()
+    Rand.load_file('randpool.dat', -1) 
+    test()
+    Rand.save_file('randpool.dat')
