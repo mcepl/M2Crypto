6d1050fb199179239c06f3413bf4040b4b9219a7
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2000-12-13 15:41:25 +0000

    Made compatible with 0.05p1.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@90 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/m2xmlrpclib.py
+++ M2Crypto/m2xmlrpclib.py
@@ -2,14 +2,14 @@
 
 Copyright (c) 1999 Ng Pheng Siong. All rights reserved. """
 
-RCS_id='$Id: m2xmlrpclib.py,v 1.2 2000/04/01 14:54:18 ngps Exp $'
+RCS_id='$Id: m2xmlrpclib.py,v 1.3 2000/12/13 15:41:25 ngps Exp $'
 
-import base64, string
+import base64, string, sys
 
 from xmlrpclib import *
-import SSL, httpslib, urllib2
+import SSL, httpslib, m2urllib
 
-__version__='0.03'
+__version__='0.05p2'
 
 class SSL_Transport(Transport):
 
@@ -23,9 +23,14 @@ class SSL_Transport(Transport):
 
     def request(self, host, handler, request_body):
         # Handle username and password.
-        user_passwd, host_port = urllib2.splituser(host)
-        _host, _port = urllib2.splitport(host_port)
-        h = httpslib.HTTPS(self.ssl_ctx, _host, int(_port))
+        user_passwd, host_port = m2urllib.splituser(host)
+        _host, _port = m2urllib.splitport(host_port)
+        if sys.version[:3] == '2.0':
+            h = httpslib.HTTPS(_host, int(_port), ssl_context=self.ssl_ctx)
+        elif sys.version[:3] ==  '1.5':
+            h = httpslib.HTTPS(self.ssl_ctx, _host, int(_port))
+        else:
+            raise RuntimeError, 'unsupported Python version'
         h.set_debuglevel(1)
 
         # What follows is as in xmlrpclib.Transport. (Except the authz bit.)
