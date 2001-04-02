14efb3585c98d247259c4ed356d58cb6a6bfa905
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2001-04-02 13:08:24 +0000

    Initial revision
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@96 2715db39-9adf-0310-9c64-84f055769b4b

---
+++ demo/smime.howto/sign.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""S/MIME HOWTO demo program.
+
+Copyright (c) 1999-2001 Ng Pheng Siong. All rights reserved."""
+
+RCS_id='$Id: sign.py,v 1.1 2001/04/02 13:08:10 ngps Exp $'
+
+from M2Crypto import BIO, Rand, SMIME
+
+def makebuf(text):
+    return BIO.MemoryBuffer(text)
+
+# Make a MemoryBuffer of the message.
+buf = makebuf('a sign of our times')
+
+# Seed the PRNG.
+Rand.load_file('randpool.dat', -1)
+
+# Instantiate an SMIME object; set it up; sign the buffer.
+s = SMIME.SMIME()
+s.load_key('signer_key.pem', 'signer.pem')
+p7 = s.sign(buf)
+
+# Recreate buf.
+buf = makebuf('a sign of our times')
+
+# Output p7 in mail-friendly format.
+out = BIO.MemoryBuffer()
+out.write('From: sender@example.dom\n')
+out.write('To: recipient@example.dom\n')
+out.write('Subject: M2Crypto S/MIME testing\n')
+s.write(out, p7, buf)
+
+print out.read()
+
+# Save the PRNG's state.
+Rand.save_file('randpool.dat')
+
