a3967db60f2f2dde1c31da0d982afda9aa500bf6
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2001-06-03 04:42:29 +0000

    Switch to blocking before connect(), then non-blocking after. (Feels like a kludge.)
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@106 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/SSL/ssl_dispatcher.py
+++ M2Crypto/SSL/ssl_dispatcher.py
@@ -1,14 +1,13 @@
 """Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: ssl_dispatcher.py,v 1.2 2000/04/01 14:55:32 ngps Exp $'
+RCS_id='$Id: ssl_dispatcher.py,v 1.3 2001/06/03 04:42:29 ngps Exp $'
 
 # Python
 import asyncore, socket
 
 # M2Crypto
 from Connection import Connection
-from M2Crypto import Err, M2Crypto
-m2 = M2Crypto
+from M2Crypto import Err, m2
 
 
 class ssl_dispatcher(asyncore.dispatcher):
@@ -17,8 +16,13 @@ class ssl_dispatcher(asyncore.dispatcher):
         self.family_and_type=socket.AF_INET, socket.SOCK_STREAM
         self.ssl_ctx=ssl_context
         self.socket=Connection(self.ssl_ctx)
-        self.socket.setblocking(0)
+        #self.socket.setblocking(0)
         self.add_channel()
+
+    def connect(self, addr):
+        self.socket.setblocking(1)
+        self.socket.connect(addr)
+        self.socket.setblocking(0)
 
     def recv(self, size=4096):
         return self.socket.recv(size)
