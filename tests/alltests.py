6909c6bb699ff1680589a060851c839febb2890a
Ng Pheng Siong <ngps@netmemetic.com>
2001-07-22 08:25:30 +0000

    Conditionally include tests that run on Unix only.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@114 2715db39-9adf-0310-9c64-84f055769b4b

--- tests/alltests.py
+++ tests/alltests.py
@@ -1,20 +1,28 @@
-#!/usr/bin/env python2.0
+#!/usr/bin/env python
 
-RCS_id = '$Id: alltests.py,v 1.1 2000/11/08 14:39:45 ngps Exp $'
+RCS_id = '$Id: alltests.py,v 1.2 2001/07/22 08:25:30 ngps Exp $'
 
-import unittest
+import os, unittest
 
 def suite():
-    modules_to_test = ('test_bio_membuf',
+    modules_to_test = ['test_bio_membuf',
         'test_bio_file',
-        'test_bio_iobuf') 
+        'test_bio_iobuf',
+        'test_dh',
+        'test_dsa',
+        'test_rsa'] 
+    if os.name == 'posix':
+        modules_to_test.append('test_ssl')
     alltests = unittest.TestSuite()
     for module in map(__import__, modules_to_test):
         alltests.addTest(module.suite())
     return alltests
 
 if __name__ == '__main__':
+    from M2Crypto import Rand
     import sys
+    Rand.load_file('randpool.dat', -1) 
     if not unittest.TextTestRunner().run(suite()).wasSuccessful():
         sys.exit(1)
+    Rand.save_file('randpool.dat')
 
