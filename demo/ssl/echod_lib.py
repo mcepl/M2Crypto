5ce5de8d2e6a88c5994f1f8f08138649500d20d6
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2001-09-18 12:17:57 +0000

    Better error handling in the echo loop.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@117 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/ssl/echod_lib.py
+++ demo/ssl/echod_lib.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-
 """Support routines for the various SSL 'echo' servers.
 
-Copyright (c) 1999-2000 Ng Pheng Siong. All rights reserved."""
+Copyright (c) 1999-2001 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: echod_lib.py,v 1.3 2000/08/23 15:40:37 ngps Exp $'
+RCS_id='$Id: echod_lib.py,v 1.4 2001/09/18 12:17:57 ngps Exp $'
 
 import SocketServer
 from M2Crypto import Err, SSL
@@ -15,9 +13,9 @@ def init_context(protocol, certfile, cafile, verify, verify_depth=10):
     ctx.load_client_ca(cafile)
     ctx.load_verify_info(cafile)
     ctx.set_verify(verify, verify_depth)
-    #ctx.set_allow_unknown_ca(1)
+    ctx.set_allow_unknown_ca(1)
     ctx.set_session_id_ctx('echod')
-    #ctx.set_info_callback()
+    ctx.set_info_callback()
     return ctx
 
 
@@ -32,13 +30,18 @@ class ssl_echo_handler(SocketServer.BaseRequestHandler):
             print 'Client Subject   =', peer.get_subject().CN
         self.request.write(self.buffer)
         while 1:
-            buf = self.request.read()
-            if not buf:
-                break
-            self.request.write(buf) 
+            try:
+                buf = self.request.read()
+                if not buf:
+                    break
+                self.request.write(buf) 
+            except SSL.SSLError, what:
+                if str(what) == 'unexpected eof':
+                    break
+                else:
+                    raise
 
     def finish(self):
-        self.request.set_shutdown(SSL.SSL_SENT_SHUTDOWN|SSL.SSL_RECEIVED_SHUTDOWN)
         self.request.close()
 
 
