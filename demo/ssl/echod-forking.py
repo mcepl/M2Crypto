556bedf57c57bc1a2ba4b7f216e164d19c363c20
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2001-09-18 12:22:38 +0000

    Cleaned up the code wrt threading.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@118 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/ssl/echod-forking.py
+++ demo/ssl/echod-forking.py
@@ -2,11 +2,11 @@
 
 """A forking SSL 'echo' server.
 
-Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
+Copyright (c) 1999-2001 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: echod-forking.py,v 1.4 2000/08/23 15:45:41 ngps Exp $'
+RCS_id='$Id: echod-forking.py,v 1.5 2001/09/18 12:22:38 ngps Exp $'
 
-from M2Crypto import DH, Rand, SSL, threading
+from M2Crypto import DH, Rand, SSL
 import echod_lib
 
 class ssl_echo_handler(echod_lib.ssl_echo_handler):
@@ -15,11 +15,9 @@ class ssl_echo_handler(echod_lib.ssl_echo_handler):
 
 if __name__=='__main__':
     Rand.load_file('../randpool.dat', -1) 
-    threading.init()
     ctx=echod_lib.init_context('sslv23', 'server.pem', 'ca.pem', SSL.verify_peer)
     ctx.set_tmp_dh('dh1024.pem')
     s=SSL.ForkingSSLServer(('', 9999), ssl_echo_handler, ctx)
     s.serve_forever()   
-    threading.cleanup()
     Rand.save_file('../randpool.dat')
 
--- demo/ssl/echod-iterative.py
+++ demo/ssl/echod-iterative.py
@@ -2,11 +2,11 @@
 
 """A simple iterative SSL 'echo' server.
 
-Copyright (c) 1999 Ng Pheng Siong. All rights reserved."""
+Copyright (c) 1999-2001 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: echod-iterative.py,v 1.4 2000/08/23 15:46:21 ngps Exp $'
+RCS_id='$Id: echod-iterative.py,v 1.5 2001/09/18 12:19:59 ngps Exp $'
 
-from M2Crypto import Rand, SSL, threading
+from M2Crypto import Rand, SSL
 import echod_lib
 
 class ssl_echo_handler(echod_lib.ssl_echo_handler):
@@ -15,13 +15,10 @@ class ssl_echo_handler(echod_lib.ssl_echo_handler):
 
 if __name__=='__main__':
     Rand.load_file('../randpool.dat', -1) 
-    threading.init()
     ctx=echod_lib.init_context('sslv23', 'server.pem', 'ca.pem', \
-        #SSL.verify_peer | SSL.verify_fail_if_no_peer_cert)
-        SSL.verify_none)
+        SSL.verify_peer | SSL.verify_fail_if_no_peer_cert)
     ctx.set_tmp_dh('dh1024.pem')
     s=SSL.SSLServer(('', 9999), ssl_echo_handler, ctx)
     s.serve_forever()   
-    threading.cleanup()
     Rand.save_file('../randpool.dat')
 
