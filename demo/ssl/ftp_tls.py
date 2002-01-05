74dffbe758664f1f588efe36bd7cac8b7575f577
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2002-01-05 07:30:23 +0000

    Initial revision
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@126 2715db39-9adf-0310-9c64-84f055769b4b

---
+++ demo/ssl/ftp_tls.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""Demo for M2Crypto.ftpslib's FTP/TLS client.
+
+This client interoperates with M2Crypto's Medusa-based FTP/TLS
+server as well as Peter Runestig's patched-for-TLS OpenBSD FTP 
+server.
+
+Copyright (c) 1999-2002 Ng Pheng Siong. All rights reserved."""
+
+_RCS_id = '$Id: ftp_tls.py,v 1.1 2002/01/05 07:30:23 ngps Exp $'
+
+from M2Crypto import SSL, ftpslib
+
+def passive():
+    ctx = SSL.Context('sslv23')
+    f = ftpslib.FTP_TLS(ssl_ctx=ctx)
+    f.set_pasv(1)
+    f.connect('127.0.0.1', 9021)
+    f.auth_tls()
+    f.login('ftp', 'ngps@')
+    f.prot_p()
+    f.retrlines('LIST')
+    f.quit()
+
+def active():
+    ctx = SSL.Context('sslv23')
+    f = ftpslib.FTP_TLS(ssl_ctx=ctx)
+    f.set_pasv(0)
+    f.connect('127.0.0.1', 9021)
+    f.auth_tls()
+    f.login('ftp', 'ngps@')
+    f.prot_p()
+    f.retrlines('LIST')
+    f.quit()
+
+
+if __name__ == '__main__':
+    passive()
+    active()
+
