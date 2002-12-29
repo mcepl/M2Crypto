9f142058a14d3d2deb00701f2ee3c135b983d423
Ng Pheng Siong <ngps@netmemetic.com>
2002-12-29 12:44:33 +0000

    Updated for SWIG 1.3.17.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@153 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/m2.py
+++ M2Crypto/m2.py
@@ -2,9 +2,9 @@
 
 Copyright (c) 1999-2003 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: m2.py,v 1.2 2002/12/23 03:51:07 ngps Exp $'
+RCS_id='$Id: m2.py,v 1.3 2002/12/29 12:44:33 ngps Exp $'
 
-from _m2crypto import *
+from __m2crypto import *
 lib_init()
 
 
