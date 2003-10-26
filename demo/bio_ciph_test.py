cc58b17506694ba74a11242e020723b8d80957c2
Ng Pheng Siong <ngps@netmemetic.com>
2003-10-26 13:20:30 +0000

    Comment out RC5.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@191 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/bio_ciph_test.py
+++ demo/bio_ciph_test.py
@@ -1,7 +1,7 @@
 """BIO cipher filtering demonstration.
 Copyright (c) 1999-2003 Ng Pheng Siong. All rights reserved."""
 
-RCS_id='$Id: bio_ciph_test.py,v 1.3 2003/05/11 16:13:23 ngps Exp $'
+RCS_id='$Id: bio_ciph_test.py,v 1.4 2003/10/26 13:20:30 ngps Exp $'
 
 from M2Crypto import BIO, Rand, m2
 
@@ -35,7 +35,7 @@ if __name__=='__main__':
     ciphers=['bf_ecb', 'bf_cbc', 'bf_cfb', 'bf_ofb',\
         #'idea_ecb', 'idea_cbc', 'idea_cfb', 'idea_ofb',\
         'cast5_ecb', 'cast5_cbc', 'cast5_cfb', 'cast5_ofb',\
-        'rc5_ecb', 'rc5_cbc', 'rc5_cfb', 'rc5_ofb',\
+        #'rc5_ecb', 'rc5_cbc', 'rc5_cfb', 'rc5_ofb',\
         'des_ecb', 'des_cbc', 'des_cfb', 'des_ofb',\
         'des_ede_ecb', 'des_ede_cbc', 'des_ede_cfb', 'des_ede_ofb',\
         'des_ede3_ecb', 'des_ede3_cbc', 'des_ede3_cfb', 'des_ede3_ofb',\
@@ -45,7 +45,6 @@ if __name__=='__main__':
         'rc4', 'rc2_40_cbc']
     Rand.load_file('randpool.dat', -1) 
     for i in ciphers:
-        #test_c(i)
         test_py(i)
     Rand.save_file('randpool.dat')
 
