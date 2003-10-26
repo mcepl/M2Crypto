af17313cd9c233090b97f91c5f3704546b3c3b18
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2003-10-26 13:15:31 +0000

    Interim checkin for 0.12.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@186 2715db39-9adf-0310-9c64-84f055769b4b

--- setup.py
+++ setup.py
@@ -6,12 +6,62 @@ Distutils installer for M2Crypto.
 Copyright (c) 1999-2003, Ng Pheng Siong. All rights reserved.
 """
 
-_RCS_id = '$Id: setup.py,v 1.8 2003/06/30 06:13:11 ngps Exp $'
+_RCS_id = '$Id: setup.py,v 1.9 2003/10/26 13:15:31 ngps Exp $'
 
 import os, shutil
 from distutils.core import setup, Extension
+from distutils.command import build_ext
 
-# Set up paths.
+# This copy of swig_sources is from Python 2.2.
+
+def swig_sources (self, sources):
+
+    """Walk the list of source files in 'sources', looking for SWIG
+    interface (.i) files.  Run SWIG on all that are found, and
+    return a modified 'sources' list with SWIG source files replaced
+    by the generated C (or C++) files.
+    """
+
+    new_sources = []
+    swig_sources = []
+    swig_targets = {}
+
+    # XXX this drops generated C/C++ files into the source tree, which
+    # is fine for developers who want to distribute the generated
+    # source -- but there should be an option to put SWIG output in
+    # the temp dir.
+
+    if self.swig_cpp:
+        target_ext = '.cpp'
+    else:
+        target_ext = '.c'
+
+    for source in sources:
+        (base, ext) = os.path.splitext(source)
+        if ext == ".i":             # SWIG interface file
+            new_sources.append(base + target_ext)
+            swig_sources.append(source)
+            swig_targets[source] = new_sources[-1]
+        else:
+            new_sources.append(source)
+
+    if not swig_sources:
+        return new_sources
+
+    swig = self.find_swig()
+    swig_cmd = [swig, "-python", "-ISWIG"]
+    if self.swig_cpp:
+        swig_cmd.append("-c++")
+
+    for source in swig_sources:
+        target = swig_targets[source]
+        self.announce("swigging %s to %s" % (source, target))
+        self.spawn(swig_cmd + ["-o", target, source])
+
+    return new_sources
+
+build_ext.build_ext.swig_sources = swig_sources
+
 my_inc = os.path.join(os.getcwd(), 'SWIG')
 
 if os.name == 'nt':
@@ -19,7 +69,6 @@ if os.name == 'nt':
     include_dirs = [my_inc, openssl_dir + '/include']
     library_dirs = [openssl_dir + '\\lib']
     libraries = ['ssleay32', 'libeay32']
-    #libraries = ['ssleay32_bc', 'libeay32_bc']
     extra_compile_args = [ "-DTHREADING" ]
 
 elif os.name == 'posix':
@@ -28,7 +77,6 @@ elif os.name == 'posix':
     libraries = ['ssl', 'crypto']
     extra_compile_args = [ "-DTHREADING" ]
 
-# Describe the module.
 m2crypto = Extension(name = '__m2crypto',
                      sources = ['SWIG/_m2crypto.i'],
                      include_dirs = include_dirs,
@@ -42,7 +90,7 @@ setup(name = 'M2Crypto',
     description = 'M2Crypto: A Python crypto and SSL toolkit',
     author = 'Ng Pheng Siong',
     author_email = 'ngps@netmemetic.com',
-    url = 'http://www.post1.com/home/ngps/m2/',
+    url = 'http://sandbox.rulemaker.net/ngps/m2/',
     packages = ['M2Crypto', 'M2Crypto.SSL', 'M2Crypto.PGP'],
     ext_package = 'M2Crypto',
     ext_modules = [m2crypto]
