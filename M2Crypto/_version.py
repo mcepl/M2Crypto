37dc8ea4731ab2e4f5eaef1370c4563d8e610e8c
ngps <ngps@2715db39-9adf-0310-9c64-84f055769b4b>
2004-03-31 01:30:58 +0000

    Update version to 0.13.1.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@226 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/_version.py
+++ M2Crypto/_version.py
@@ -1,8 +1,8 @@
 """M2Crypto Version Info"""
 
-RCS_id='$Id: _version.py,v 1.2 2004/03/25 06:33:29 ngps Exp $'
+RCS_id='$Id: _version.py,v 1.3 2004/03/31 01:30:58 ngps Exp $'
 
 import string
-version_info = (0, 13)
+version_info = (0, 13, 1)
 version = string.join(map(lambda x: "%s" % x, version_info), ".")
 
