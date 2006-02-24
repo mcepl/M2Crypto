5b0c06337c688a29d81d388748c433657b2b12b9
Heikki Toivonen <heikki@heikkitoivonen.net>
2006-02-24 04:46:33 +0000

    Added docstrings.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@323 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/threading.py
+++ M2Crypto/threading.py
@@ -1,4 +1,5 @@
-"""M2Crypto threading support. 
+"""
+M2Crypto threading support, required for multithreaded applications. 
 
 Copyright (c) 1999-2003 Ng Pheng Siong. All rights reserved."""
 
@@ -8,8 +9,14 @@ RCS_id='$Id$'
 import m2
 
 def init():
+    """
+    Initialize threading support.
+    """
     m2.threading_init()
 
 def cleanup():
+    """
+    End and cleanup threading support.
+    """
     m2.threading_cleanup()
 
