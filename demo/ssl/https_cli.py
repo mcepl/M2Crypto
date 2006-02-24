ea8153366231d11aeb75b6f864b97f1b83516622
heikki <heikki@2715db39-9adf-0310-9c64-84f055769b4b>
2006-02-24 23:56:46 +0000

    Get rid of pre-2.3 junk.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@353 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/ssl/https_cli.py
+++ demo/ssl/https_cli.py
@@ -10,55 +10,33 @@ import sys
 from M2Crypto import Rand, SSL, httpslib, threading
 
 
-if sys.version[:3] == '1.5':
-
-    def test_httpslib():
-        ctx = SSL.Context('sslv3')
-        #ctx.load_cert('client.pem')
-        h = httpslib.HTTPS(ctx, '127.0.0.1:9443')
-        h.putrequest('GET', '/')
-        h.putheader('Accept', 'text/html')
-        h.putheader('Accept', 'text/plain')
-        h.endheaders()
-        errcode, errmsg, headers = h.getreply()
-        f = h.getfile()
-        while 1:
-            data = f.read()   
-            if not data:
-                break
-            sys.stdout.write(data)
-        f.close()
-
-
-elif sys.version[0] == '2':
-
-    def test_httpslib():
-        ctx = SSL.Context('sslv23')
-        ctx.load_cert_chain('client.pem')
-        ctx.load_verify_locations('ca.pem', '')
-        ctx.set_verify(SSL.verify_peer, 10)        
-        ctx.set_info_callback()
-        h = httpslib.HTTPSConnection('127.0.0.1', 9443, ssl_context=ctx)
-        h.set_debuglevel(1)
-        h.putrequest('GET', '/')
-        h.putheader('Accept', 'text/html')
-        h.putheader('Accept', 'text/plain')
-        h.putheader('Connection', 'close')
-        h.endheaders()
-        resp = h.getresponse()
-        f = resp.fp
-        c = 0
-        while 1:
-            # Either of following two works.
-            #data = f.readline(4096)   
-            data = resp.read(4096)
-            if not data: break
-            c = c + len(data)
-            #print data
-            sys.stdout.write(data)
-            sys.stdout.flush()
-        f.close()
-        h.close()
+def test_httpslib():
+    ctx = SSL.Context('sslv23')
+    ctx.load_cert_chain('client.pem')
+    ctx.load_verify_locations('ca.pem', '')
+    ctx.set_verify(SSL.verify_peer, 10)        
+    ctx.set_info_callback()
+    h = httpslib.HTTPSConnection('127.0.0.1', 9443, ssl_context=ctx)
+    h.set_debuglevel(1)
+    h.putrequest('GET', '/')
+    h.putheader('Accept', 'text/html')
+    h.putheader('Accept', 'text/plain')
+    h.putheader('Connection', 'close')
+    h.endheaders()
+    resp = h.getresponse()
+    f = resp.fp
+    c = 0
+    while 1:
+        # Either of following two works.
+        #data = f.readline(4096)   
+        data = resp.read(4096)
+        if not data: break
+        c = c + len(data)
+        #print data
+        sys.stdout.write(data)
+        sys.stdout.flush()
+    f.close()
+    h.close()
 
 if __name__=='__main__':
     Rand.load_file('../randpool.dat', -1) 
