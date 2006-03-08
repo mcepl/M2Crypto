d122cb4ab947b2876b8b379d5d309f6e2516e936
heikki <heikki@2715db39-9adf-0310-9c64-84f055769b4b>
2006-03-08 22:52:48 +0000

    Bug 5314, unit tests to show proxy certificate handling. Original patch by Matt Rodriguez.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@374 2715db39-9adf-0310-9c64-84f055769b4b

--- tests/test_x509.py
+++ tests/test_x509.py
@@ -82,24 +82,25 @@ class X509TestCase(unittest.TestCase):
         issuer.O = 'The Organization Otherwise Known as My CA, Inc.'
         cert.set_issuer(issuer)
         cert.set_pubkey(pkey)
-        cert.set_pubkey(cert.get_pubkey())
+        cert.set_pubkey(cert.get_pubkey()) # Make sure get/set work
         ext = X509.new_extension('subjectAltName', 'DNS:foobar.example.com')
         ext.set_critical(0)
         cert.add_ext(ext)
         cert.sign(pk, 'sha1')
-        #assert not cert.check_ca()
         assert(cert.get_ext('subjectAltName').get_name() == 'subjectAltName')
         assert(cert.get_ext_at(0).get_name() == 'subjectAltName')
         assert(cert.get_ext_at(0).get_value() == 'DNS:foobar.example.com')
+        assert cert.get_ext_count() == 1, cert.get_ext_count()
         assert cert.verify()
         assert cert.verify(pkey)
+        assert cert.verify(cert.get_pubkey())
         
         if m2.OPENSSL_VERSION_NUMBER >= 0x90800f:
             assert not cert.check_ca()
         else:
             self.assertRaises(AttributeError, cert.check_ca)
 
-    def check_mkcacert(self):
+    def mkcacert(self):
         req, pk = self.mkreq(512, ca=1)
         pkey = req.get_pubkey()
         sub = req.get_subject()
@@ -118,8 +119,7 @@ class X509TestCase(unittest.TestCase):
         issuer.C = "UK"
         issuer.CN = "OpenSSL Group"
         cert.set_issuer(issuer)
-        cert.set_pubkey(pkey)
-        cert.set_pubkey(cert.get_pubkey())
+        cert.set_pubkey(pkey) 
         ext = X509.new_extension('basicConstraints', 'CA:TRUE')
         cert.add_ext(ext)
         cert.sign(pk, 'sha1')
@@ -128,7 +128,81 @@ class X509TestCase(unittest.TestCase):
             assert cert.check_ca()
         else:
             self.assertRaises(AttributeError, cert.check_ca)
+        
+        return cert, pk, pkey
 
+    def check_mkcacert(self): 
+        cacert, pk, pkey = self.mkcacert()
+        assert cacert.verify(pkey)
+        
+
+    def check_mkproxycert(self): 
+        cacert, pk1, pkey = self.mkcacert()
+        end_entity_cert_req, pk2 = self.mkreq(512)
+        end_entity_cert = self.make_eecert(cacert)
+        end_entity_cert.set_subject(end_entity_cert_req.get_subject())
+        end_entity_cert.set_pubkey(end_entity_cert_req.get_pubkey())
+        end_entity_cert.sign(pk1, 'sha1')
+        proxycert = self.make_proxycert(end_entity_cert)
+        proxycert.sign(pk2, 'sha1')
+        assert proxycert.verify(pk2)
+        assert proxycert.get_ext_at(0).get_name() == 'proxyCertInfo', proxycert.get_ext_at(0).get_name()
+        assert proxycert.get_ext_at(0).get_value() == 'Path Length Constraint: infinite\nPolicy Language: Inherit all\n', '"%s"' % proxycert.get_ext_at(0).get_value()
+        assert proxycert.get_ext_count() == 1, proxycert.get_ext_count()
+        assert proxycert.get_subject().as_text() == 'C=UK, CN=OpenSSL Group, CN=Proxy', proxycert.get_subject().as_text()
+
+    def make_eecert(self, cacert):
+        eecert = X509.X509()
+        eecert.set_serial_number(2)
+        eecert.set_version(2)
+        t = long(time.time()) + time.timezone
+        now = ASN1.ASN1_UTCTIME()
+        now.set_time(t)
+        now_plus_year = ASN1.ASN1_UTCTIME()
+        now_plus_year.set_time(t + 60 * 60 * 24 * 365)
+        eecert.set_not_before(now)
+        eecert.set_not_after(now_plus_year)
+        eecert.set_issuer(cacert.get_subject())
+        return eecert
+    
+    def make_proxycert(self, eecert):
+        proxycert = X509.X509()
+        pk2 = EVP.PKey()
+        proxykey =  RSA.gen_key(512, 65537, self.callback)
+        pk2.assign_rsa(proxykey)
+        proxycert.set_pubkey(pk2)
+        proxycert.set_version(2)
+        not_before = ASN1.ASN1_UTCTIME()
+        not_after = ASN1.ASN1_UTCTIME()
+        not_before.set_time(int(time.time()))
+        offset = 12 * 3600
+        not_after.set_time(int(time.time()) + offset )
+        proxycert.set_not_before(not_before)
+        proxycert.set_not_after(not_after)
+        proxycert.set_issuer_name(eecert.get_subject())
+        proxycert.set_serial_number(12345678)
+        proxy_subject_name = X509.X509_Name()
+        issuer_name_string = eecert.get_subject().as_text()
+        seq = issuer_name_string.split(",")
+
+        subject_name = X509.X509_Name()
+        for entry in seq:
+            l = entry.split("=")
+            subject_name.add_entry_by_txt(field=l[0].strip(),
+                                          type=ASN1.MBSTRING_ASC,
+                                          entry=l[1], len=-1, loc=-1, set=0)
+
+        subject_name.add_entry_by_txt(field="CN", type=ASN1.MBSTRING_ASC,
+                                      entry="Proxy", len=-1, loc=-1, set=0)
+
+
+        proxycert.set_subject_name(subject_name)
+        pci_ext = X509.new_extension("proxyCertInfo", 
+                                     "critical,language:Inherit all", 1, 0)
+        proxycert.add_ext(pci_ext)
+        return proxycert
+ 
+ 
 class X509_StackTestCase(unittest.TestCase):
     
     def check_make_stack_from_der(self):
@@ -188,10 +262,10 @@ class X509_StackTestCase(unittest.TestCase):
         
 
 def suite():
-     suite = unittest.TestSuite()
-     suite.addTest(unittest.makeSuite(X509TestCase, 'check'))
-     suite.addTest(unittest.makeSuite(X509_StackTestCase, 'check'))
-     return suite
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(X509TestCase, 'check'))
+    suite.addTest(unittest.makeSuite(X509_StackTestCase, 'check'))
+    return suite
 
 
 if __name__ == '__main__':
