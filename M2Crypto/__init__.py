53f3e6937519926387af4ea76b2a648513555819
heikki <heikki@2715db39-9adf-0310-9c64-84f055769b4b>
2006-10-30 23:09:10 +0000

    Bug 7083, current m2urllib2 implementation requires Python 2.4 so make inclusion of that be dependent on Python version.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@475 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/__init__.py
+++ M2Crypto/__init__.py
@@ -33,7 +33,12 @@ import PGP
 import m2urllib
 # Backwards compatibility.
 urllib2 = m2urllib
-import m2urllib2
+
+import sys
+if sys.version_info >= (2,4):
+    import m2urllib2
+del sys
+
 import ftpslib
 import httpslib
 import m2xmlrpclib
