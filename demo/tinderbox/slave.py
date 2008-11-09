48df7dd2933ad6220584ea05fa85037b11674f9f
heikki <heikki@2715db39-9adf-0310-9c64-84f055769b4b>
2008-11-09 07:26:56 +0000

    Read settings from config file, default to silent operation (but change debug_script to True and you get output), print to log the commands we will execute, fix email headers.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@649 2715db39-9adf-0310-9c64-84f055769b4b

--- demo/tinderbox/slave.py
+++ demo/tinderbox/slave.py
@@ -1,69 +1,134 @@
 #!/usr/bin/env python
 #
-# This is a sample Tinderbox2 buildslave script
-#
+"""
+This is a sample Tinderbox2 buildslave script. Run it from cron,
+or modify the section at the bottom to run in a loop.
 
-# XXX read from an ini file
-BUILD_NAME = 'example-linux'
-TO_EMAIL = 'builds@example.com'
-FROM_EMAIL = 'myemail@example.com'
-SMTP_USER = 'myemail'
-SMTP_PASSWORD = 'secret'
-SMTP_SERVER = 'smtp.example.com'
-SMTP_PORT = 587
+NOTE: WAIT at least 6 minutes after the last build before starting
+      the next build!
 
-import time, smtplib, os
+Create config.ini file with the following contents:
+
+[build]
+name = identify your build slave, for example Ubuntu 8.04 32-bit
+;;optional fields:
+;;uname = uname -a
+;;swig = swig -version
+;;cc = gcc --version
+;;openssl = openssl version
+;;python = python --version
+;;svn = svn co http://svn.osafoundation.org/m2crypto/trunk m2crypto
+;;build = python setup.py clean --all build
+;;test = python setup.py test
+
+[email]
+from = your email
+to = Email Heikki Toivonen to get the address
+user = smtp username
+password = smtp password
+server = smtp server
+port = smtp port
+"""
+
+import time, smtplib, os, ConfigParser
 import build_lib as bl
+
+# Change to True when you are troubleshooting this build script
+debug_script = False
 
 # These commands assume we are running on a unix-like system where default
 # build options work and all prerequisites are installed and in PATH etc.
-commands = [
-  ['uname', '-a'],
-  ['swig', '-version'],
-  ['openssl', 'version'],
-  ['python', '--version'],
-  ['svn', 'co', 'http://svn.osafoundation.org/m2crypto/trunk', 'm2crypto'],
-  ['python', 'setup.py', 'clean', '--all', 'build'],
-  ['python', 'setup.py', 'test']
-]
+DEFAULT_COMMANDS = {
+  'uname': ['uname', '-a'],
+  'swig': ['swig', '-version'],
+  'cc': ['gcc', '--version'],
+  'openssl': ['openssl', 'version'],
+  'python': ['python', '--version'],
+  'svn': ['svn', 'co', 'http://svn.osafoundation.org/m2crypto/trunk', 'm2crypto'],
+  'build': ['python', 'setup.py', 'clean', '--all', 'build'],
+  'test': ['python', 'setup.py', 'test']
+}
 
-status = 'success'
+def load_config(cfg='config.ini'):
+    config = {}
+    cp = ConfigParser.ConfigParser()
+    cp.read(cfg)
+    for section in cp.sections():
+        for option in cp.options(section):
+            config[option] = cp.get(section, option).strip()
+    return config
 
-cwd = os.getcwd()
 
-bl.initLog(None)
-
-starttime = int(time.time())
-
-for command in commands:
-    if bl.runCommand(command, timeout=120):
-        if command[-1] == 'test': # XXX
-            status = 'test_failed'
-        else:
-            status = 'build_failed'
-        break
-    if command[0] == 'svn': # XXX
-        os.chdir('m2crypto')
+def build(commands, config):
+    status = 'success'
     
-timenow = int(time.time())
+    cwd = os.getcwd()
+    
+    bl.initLog('tbox.log', echo=debug_script)
+    
+    starttime = int(time.time())
+    
+    for command in commands:
+        cmd = config.get(command) 
+        if not cmd:
+            cmd = DEFAULT_COMMANDS[command]
+        else:
+            cmd = cmd.split()
+        
+        bl.log('*** ' + ' '.join(cmd))
+        if bl.runCommand(cmd, timeout=120):
+            if command == 'test':
+                status = 'test_failed'
+            else:
+                status = 'build_failed'
+            break
+            import sys;sys.exit(1)
+        if command == 'svn':
+            os.chdir('m2crypto')
+        
+    timenow = int(time.time())
+    
+    bl.closeLog()
+    
+    os.chdir(cwd)
 
-os.chdir(cwd)
+    return 'tbox.log', starttime, timenow, status
 
-msg = """tinderbox: tree: M2Crypto
+
+def email(logpath, starttime, timenow, status, config):
+    msg = """From: %(from)s
+To: %(to)s
+Subject: tree: M2Crypto
+
+
+tinderbox: tree: M2Crypto
 tinderbox: starttime: %(starttime)d
 tinderbox: timenow: %(timenow)d
-tinderbox: status: success
+tinderbox: status: %(status)s
 tinderbox: buildname: %(buildname)s
 tinderbox: errorparser: unix
 tinderbox: END
 
-""" % {'starttime': starttime, 'timenow': timenow, 'buildname': BUILD_NAME}
+""" % {'from': config['from'], 'to': config['to'], 
+           'starttime': starttime, 'timenow': timenow,
+           'status': status,
+           'buildname': config['name']}
+    
+    msg += open(logpath).read()
+    
+    server = smtplib.SMTP(host=config['server'], port=int(config['port']))
+    if debug_script:
+        server.set_debuglevel(1)
+    server.starttls() # if your server supports STARTTLS
+    server.login(config['user'], config['password'])
+    server.sendmail(config['from'], config['to'], msg)
+    server.quit()
 
-msg += open('tbox.log').read()
 
-server = smtplib.SMTP(host=SMTP_SERVER, port=SMTP_PORT)
-server.set_debuglevel(1)
-server.starttls()
-server.login(SMTP_USER, SMTP_PASSWORD)
-server.sendmail(FROM_EMAIL, TO_EMAIL, msg)
-server.quit()
+if __name__ == '__main__':
+    config = load_config()    
+    
+    commands = ['uname', 'swig', 'cc', 'openssl', 'python', 'svn', 'build', 'test']
+
+    logpath, starttime, timenow, status = build(commands, config)
+    email(logpath, starttime, timenow, status, config)
