09b6e20f0d6380b440e0f6455f61ec080c3a938f
heikki <heikki@2715db39-9adf-0310-9c64-84f055769b4b>
2008-11-26 08:18:17 +0000

    Use assertEqual so that we can actually see how the values are different.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@658 2715db39-9adf-0310-9c64-84f055769b4b

--- tests/test_asn1.py
+++ tests/test_asn1.py
@@ -41,19 +41,19 @@ class ASN1TestCase(unittest.TestCase):
         #assert str(asn1) == 'Aug  7 05:30:11 1999 GMT'
         t1 = time.strptime(str(asn1), format)
         t2 = time.strptime(s, utcformat)
-        assert t1 == t2
+        self.assertEqual(t1, t2)
         
         asn1.set_time(500)
         #assert str(asn1) == 'Jan  1 00:08:20 1970 GMT'
         t1 = time.strftime(format, time.strptime(str(asn1), format))
         t2 = time.strftime(format, time.gmtime(500))
-        assert t1 == t2
+        self.assertEqual(t1, t2)
         
         t = long(time.time()) + time.timezone
         asn1.set_time(t)
         t1 = time.strftime(format, time.strptime(str(asn1), format))
         t2 = time.strftime(format, time.gmtime(t))
-        assert t1 == t2
+        self.assertEqual(t1, t2)
          
 
 def suite():
