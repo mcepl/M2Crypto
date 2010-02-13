a371ee738222b70c236ba10ebdc3adf84e9c6f97
heikki <heikki@2715db39-9adf-0310-9c64-84f055769b4b>
2010-02-13 06:44:04 +0000

    Bug 12910, make httpslib.ProxyHTTPSConnection work with Python 2.3.
    git-svn-id: http://svn.osafoundation.org/m2crypto/trunk@722 2715db39-9adf-0310-9c64-84f055769b4b

--- M2Crypto/httpslib.py
+++ M2Crypto/httpslib.py
@@ -145,7 +145,10 @@ class ProxyHTTPSConnection(HTTPSConnection):
         self._real_host = host
         self._real_port = int(port)
         rest = urlunsplit((None, None, path, query, fragment))
-        HTTPSConnection.putrequest(self, method, rest, skip_host, skip_accept_encoding)
+        if sys.version_info < (2,4):
+            HTTPSConnection.putrequest(self, method, rest, skip_host)
+        else:
+            HTTPSConnection.putrequest(self, method, rest, skip_host, skip_accept_encoding)
 
     def putheader(self, header, value):
         # Store the auth header if passed in.
