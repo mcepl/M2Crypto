0f195b9f973eaa7c89c4d88ae76f084a7d1a8738
Miloslav Trmač <mitr@redhat.com>
2011-05-10 15:12:41 +0200

    Fix handling of buffer() objects as input data to SSL
    See https://bugzilla.redhat.com/show_bug.cgi?id=702766 for more
    information.
    
    Accommodation of M2Crypto to the incompatible C API changes caused by
    https://docs.python.org/2.6/whatsnew/2.6.html#pep-3118-revised-buffer-protocol.
    
    Probably caused by changes introduced in 8cdcb2308a
    
    This is a reproducer from the bug:
    
        import mmap
        import os
        import M2Crypto
    
        if __name__ == '__main__':
            filename = '/etc/issue'
    
            fd = open(filename, 'rb')
            mapped = mmap.mmap(fd.fileno(),
                               os.path.getsize(filename),
                               mmap.MAP_SHARED,
                               mmap.PROT_READ)
            buf = buffer(mapped)
    
            c = M2Crypto.m2.ssl_ctx_new(M2Crypto.m2.tlsv1_method())
            s = M2Crypto.m2.ssl_new(c)
            M2Crypto.m2.ssl_write(s, buf)

--- SWIG/_lib.h
+++ SWIG/_lib.h
@@ -7,6 +7,16 @@ typedef int Py_ssize_t;
 #define PY_SSIZE_T_MIN INT_MIN
 #endif
 
+#if PY_VERSION_HEX < 0x02060000
+struct Py_buffer /* Only a subset */
+{
+  void *buf;
+  Py_ssize_t len;
+};
+
+#define PyBUF_CONTIG_RO 0
+#endif /* PY_VERSION_HEX < 0x02060000 */
+
 typedef struct _blob {
 	unsigned char *data;
 	int len;
@@ -19,6 +29,10 @@ void blob_free(Blob *blob);
 static int m2_PyObject_AsReadBufferInt(PyObject *obj, const void **buffer,
                                        int *buffer_len);
 static int m2_PyString_AsStringAndSizeInt(PyObject *obj, char **s, int *len);
+
+/* Always use these two together, to correctly handle non-memoryview objects. */
+static int m2_PyObject_GetBufferInt(PyObject *obj, Py_buffer *view, int flags);
+static void m2_PyBuffer_Release(PyObject *obj, Py_buffer *view);
 
 void gen_callback(int p, int n, void *arg);
 int passphrase_callback(char *buf, int num, int v, void *userdata);
