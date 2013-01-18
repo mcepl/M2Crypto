f955b62be1a58f7e901f46ac181297a6f54a99c6
Matěj Cepl <mcepl@redhat.com>
2013-01-18 23:16:50 +0100

    Update fedora_setup.sh to clean after itself.
    After running the script source code should be git-clean.

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+set -eu
 # This script is meant to work around the differences on Fedora Core-based
 # distributions (Redhat, CentOS, ...) compared to other common Linux
 # distributions.
 # 
 # Usage: ./fedora_setup.sh [setup.py options]
 #
-
 arch=`uname -m`
-for i in SWIG/_{ec,evp}.i; do
-  sed -i -e "s/opensslconf\./opensslconf-${arch}\./" "$i"
+trap "mv -f SWIG/_ec.i.bak SWIG/_ec.i ; mv -f SWIG/_evp.i.bak SWIG/_evp.i" EXIT
+for IFILE in SWIG/_{ec,evp}.i ; do
+    cp -f "${IFILE}" "${IFILE}.bak"
+    sed -i -e "s/opensslconf\./opensslconf-${arch}\./" "$IFILE"
 done
 
-SWIG_FEATURES=-cpperraswarn python setup.py $*
+SWIG_FEATURES=-cpperraswarn python3 setup.py $*
 
