8d9d4240c84d251b8e53f660bcbaac3f80206af7
Martin Paljak <martin@martinpaljak.net>
2013-04-27 22:27:40 +0300

    Build system changes:  - don't have code for Python < 2.4 in setup.py  - do have a .gitignore  - setup.cfg is obsolete

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ class _M2CryptoBuildExt(build_ext.build_ext):
         self.swig_opts = ['-I%s' % i for i in self.include_dirs + \
                           [opensslIncludeDir]]
         self.swig_opts.append('-includeall')
-        #self.swig_opts.append('-D__i386__') # Uncomment for early OpenSSL 0.9.7 versions, or on Fedora Core if build fails
+        self.swig_opts.append('-modern')
         #self.swig_opts.append('-DOPENSSL_NO_EC') # Try uncommenting if you can't build with EC disabled
         
         self.include_dirs += [os.path.join(self.openssl, opensslIncludeDir),
@@ -70,61 +70,6 @@ class _M2CryptoBuildExt(build_ext.build_ext):
             self.library_dirs += [os.path.join(self.openssl, 'bin')]
                
         self.library_dirs += [os.path.join(self.openssl, opensslLibraryDir)]
-
-
-if sys.version_info < (2,4):
-
-    # This copy of swig_sources is from Python 2.2.
-
-    def swig_sources (self, sources):
-
-        """Walk the list of source files in 'sources', looking for SWIG
-        interface (.i) files.  Run SWIG on all that are found, and
-        return a modified 'sources' list with SWIG source files replaced
-        by the generated C (or C++) files.
-        """
-
-        new_sources = []
-        swig_sources = []
-        swig_targets = {}
-
-        # XXX this drops generated C/C++ files into the source tree, which
-        # is fine for developers who want to distribute the generated
-        # source -- but there should be an option to put SWIG output in
-        # the temp dir.
-
-        if self.swig_cpp:
-            target_ext = '.cpp'
-        else:
-            target_ext = '.c'
-
-        for source in sources:
-            (base, ext) = os.path.splitext(source)
-            if ext == ".i":             # SWIG interface file
-                new_sources.append(base + target_ext)
-                swig_sources.append(source)
-                swig_targets[source] = new_sources[-1]
-            else:
-                new_sources.append(source)
-
-        if not swig_sources:
-            return new_sources
-
-        swig = self.find_swig()
-        swig_cmd = [swig, "-python", "-ISWIG"]
-        if self.swig_cpp:
-            swig_cmd.append("-c++")
-
-        swig_cmd += self.swig_opts 
-
-        for source in swig_sources:
-            target = swig_targets[source]
-            self.announce("swigging %s to %s" % (source, target))
-            self.spawn(swig_cmd + ["-o", target, source])
-
-        return new_sources
-    
-    build_ext.build_ext.swig_sources = swig_sources
 
 
 m2crypto = Extension(name = 'M2Crypto.__m2crypto',
