ritter-eb/m2crypto-sane

One more RC for 0.22.6

setup.py
@@ -119,7 +119,7 @@ m2crypto = Extension(name='M2Crypto.__m2crypto',
                      )
 
 setup(name='M2Crypto',
-      version='0.22.6.rc1',
+      version='0.22.6.rc2',
       description='M2Crypto: A Python crypto and SSL toolkit',
       long_description='''\
 M2Crypto is the most complete Python wrapper for OpenSSL featuring RSA, DSA,

mcepl/M2Crypto

setup.py
@@ -119,7 +119,7 @@ m2crypto = Extension(name='M2Crypto.__m2crypto',
                      )
 
 setup(name='M2Crypto',
-      version='0.22.6.rc1',
+      version='0.22.6.rc3',
       description='M2Crypto: A Python crypto and SSL toolkit',
       long_description='''\
 M2Crypto is the most complete Python wrapper for OpenSSL featuring RSA, DSA,