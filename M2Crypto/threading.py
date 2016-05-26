6638241a58507b0d4237723a7530757a7002d99a
Matěj Cepl <mcepl@cepl.eu>
2016-05-27 01:27:23 +0200

    M2Crypto/threading.py: Add PEP-484 type hints in comments.

--- M2Crypto/threading.py
+++ M2Crypto/threading.py
@@ -8,13 +8,17 @@ Copyright (c) 1999-2003 Ng Pheng Siong. All rights reserved."""
 # M2Crypto
 from M2Crypto import m2
 
+
 def init():
+    # type: () -> None
     """
     Initialize threading support.
     """
     m2.threading_init()
 
+
 def cleanup():
+    # type: () -> None
     """
     End and cleanup threading support.
     """
