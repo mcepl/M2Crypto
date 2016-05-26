bfe686707cbbb8641f1cbc9a0e852b285bcc0502
Matěj Cepl <mcepl@cepl.eu>
2016-05-27 01:27:22 +0200

    M2Crypto/BIO.py: Add PEP-484 type hints in comments.

--- M2Crypto/BIO.py
+++ M2Crypto/BIO.py
@@ -4,11 +4,12 @@ from __future__ import absolute_import
 
 Copyright (c) 1999-2004 Ng Pheng Siong. All rights reserved."""
 
-import io
 import logging
+import io  # noqa
 
-from M2Crypto import m2
-
+from M2Crypto import m2, util
+if util.py27plus:
+    from typing import AnyStr, Callable, Iterable, Optional, Union  # noqa
 
 log = logging.getLogger('BIO')
 
@@ -26,6 +27,7 @@ class BIO:
     m2_bio_free = m2.bio_free
 
     def __init__(self, bio=None, _pyfree=0, _close_cb=None):
+        # type: (Optional[BIO], int, Optional[Callable]) -> None
         self.bio = bio
         self._pyfree = _pyfree
         self._close_cb = _close_cb
@@ -43,36 +45,41 @@ class BIO:
     bio_ptr = _ptr
 
     def fileno(self):
+        # type: () -> int
         return m2.bio_get_fd(self.bio)
 
     def readable(self):
+        # type: () -> bool
         return not self.closed
 
     def read(self, size=None):
+        # type: (int) -> Union[bytes, bytearray]
         if not self.readable():
             raise IOError('cannot read')
         if size is None:
-            buf = io.BytesIO()
+            buf = bytearray()
             while 1:
                 data = m2.bio_read(self.bio, 4096)
                 if not data:
                     break
-                buf.write(data)
-            return buf.getvalue()
+                buf += data
+            return buf
         elif size == 0:
             return ''
         elif size < 0:
             raise ValueError('read count is negative')
         else:
-            return m2.bio_read(self.bio, size)
+            return bytes(m2.bio_read(self.bio, size))
 
     def readline(self, size=4096):
+        # type: (int) -> bytes
         if not self.readable():
             raise IOError('cannot read')
         buf = m2.bio_gets(self.bio, size)
         return buf
 
     def readlines(self, sizehint='ignored'):
+        # type: (Union[AnyStr, int]) -> Iterable[bytes]
         if not self.readable():
             raise IOError('cannot read')
         lines = []
@@ -84,31 +91,50 @@ class BIO:
         return lines
 
     def writeable(self):
+        # type: () -> bool
         return (not self.closed) and (not self.write_closed)
 
     def write(self, data):
+        # type: (AnyStr) -> int
+        """
+
+        @return: either data written, or [0, -1] for nothing written,
+                 -2 not implemented
+        """
         if not self.writeable():
             raise IOError('cannot write')
+        if isinstance(data, unicode):
+            data = data.encode('utf8')
         return m2.bio_write(self.bio, data)
 
     def write_close(self):
+        # type: () -> None
         self.write_closed = 1
 
     def flush(self):
+        # type: () -> None
+        """
+
+        @return: 1 for success, and 0 or -1 for failure
+        """
         m2.bio_flush(self.bio)
 
     def reset(self):
+        # type: () -> int
         """
         Sets the bio to its initial state
+        @return: 1 for success, and 0 or -1 for failure
         """
         return m2.bio_reset(self.bio)
 
     def close(self):
+        # type: () -> None
         self.closed = 1
         if self._close_cb:
             self._close_cb()
 
     def should_retry(self):
+        # type: () -> int
         """
         Can the call be attempted again, or was there an error
         ie do_handshake
@@ -117,6 +143,7 @@ class BIO:
         return m2.bio_should_retry(self.bio)
 
     def should_read(self):
+        # type: () -> int
         """
         Returns whether the cause of the condition is the bio
         should read more data
@@ -124,6 +151,7 @@ class BIO:
         return m2.bio_should_read(self.bio)
 
     def should_write(self):
+        # type: () -> int
         """
         Returns whether the cause of the condition is the bio
         should write more data
@@ -138,7 +166,6 @@ class BIO:
 
 
 class MemoryBuffer(BIO):
-
     """
     Object interface to BIO_s_mem.
 
@@ -150,6 +177,10 @@ class MemoryBuffer(BIO):
     """
 
     def __init__(self, data=None):
+        # type: (Optional[bytes]) -> None
+        if data is not None and not isinstance(data, bytes):
+            raise TypeError(
+                "data must be bytes or None, not %s" % (type(data).__name__, ))
         BIO.__init__(self)
         self.bio = m2.bio_new(m2.bio_s_mem())
         self._pyfree = 1
@@ -157,9 +188,11 @@ class MemoryBuffer(BIO):
             m2.bio_write(self.bio, data)
 
     def __len__(self):
+        # type: () -> int
         return m2.bio_ctrl_pending(self.bio)
 
     def read(self, size=0):
+        # type: (int) -> bytes
         if not self.readable():
             raise IOError('cannot read')
         if size:
@@ -171,6 +204,7 @@ class MemoryBuffer(BIO):
     getvalue = read_all = read
 
     def write_close(self):
+        # type: () -> None
         self.write_closed = 1
         m2.bio_set_mem_eof_return(self.bio, 0)
 
@@ -187,18 +221,29 @@ class File(BIO):
     """
 
     def __init__(self, pyfile, close_pyfile=1):
+        # type: (io.BinaryIO, int) -> None
         BIO.__init__(self, _pyfree=1)
         self.pyfile = pyfile
         self.close_pyfile = close_pyfile
         self.bio = m2.bio_new_fd(pyfile.fileno(), m2.bio_noclose)
 
     def close(self):
+        # type: () -> None
         self.closed = 1
         if self.close_pyfile:
             self.pyfile.close()
 
+    def reset(self):
+        # type: () -> int
+        """
+        Sets the bio to its initial state
+        @return: 0 for success, and -1 for failure
+        """
+        return super(File, self).reset()
+
 
 def openfile(filename, mode='rb'):
+    # type: (AnyStr, AnyStr) -> File
     return File(open(filename, mode))
 
 
@@ -215,6 +260,7 @@ class IOBuffer(BIO):
     m2_bio_free = m2.bio_free
 
     def __init__(self, under_bio, mode='rwb', _pyfree=1):
+        # type: (BIO, str, int) -> None
         BIO.__init__(self, _pyfree=_pyfree)
         self.io = m2.bio_new(m2.bio_f_buffer())
         self.bio = m2.bio_push(self.io, under_bio._ptr())
@@ -226,11 +272,13 @@ class IOBuffer(BIO):
             self.write_closed = 1
 
     def __del__(self):
+        # type: () -> None
         if getattr(self, '_pyfree', 0):
             self.m2_bio_pop(self.bio)
         self.m2_bio_free(self.io)
 
     def close(self):
+        # type: () -> None
         BIO.close(self)
 
 
@@ -246,28 +294,38 @@ class CipherStream(BIO):
     m2_bio_free = m2.bio_free
 
     def __init__(self, obio):
+        # type: (BIO) -> None
         BIO.__init__(self, _pyfree=1)
         self.obio = obio
         self.bio = m2.bio_new(m2.bio_f_cipher())
         self.closed = 0
 
     def __del__(self):
+        # type: () -> None
         if not getattr(self, 'closed', 1):
             self.close()
 
     def close(self):
+        # type: () -> None
         self.m2_bio_pop(self.bio)
         self.m2_bio_free(self.bio)
         self.closed = 1
 
     def write_close(self):
+        # type: () -> None
         self.obio.write_close()
 
     def set_cipher(self, algo, key, iv, op):
+        # type: (str, AnyStr, AnyStr, int) -> None
         cipher = getattr(m2, algo, None)
         if cipher is None:
             raise ValueError('unknown cipher', algo)
-        m2.bio_set_cipher(self.bio, cipher(), key, iv, op)
+        else:
+            if not isinstance(key, bytes):
+                key = key.encode('utf8')
+            if not isinstance(iv, bytes):
+                iv = iv.encode('utf8')
+        m2.bio_set_cipher(self.bio, cipher(), key, iv, int(op))
         m2.bio_push(self.bio, self.obio._ptr())
 
 
@@ -276,11 +334,13 @@ class SSLBio(BIO):
     Object interface to BIO_f_ssl
     """
     def __init__(self, _pyfree=1):
-        BIO.__init__(self, _pyfree)
+        # type: (int) -> None
+        BIO.__init__(self, _pyfree=_pyfree)
         self.bio = m2.bio_new(m2.bio_f_ssl())
         self.closed = 0
 
     def set_ssl(self, conn, close_flag=m2.bio_noclose):
+        # type: (Connection, int) -> None
         """
         Sets the bio to the SSL pointer which is
         contained in the connection object.
@@ -291,6 +351,7 @@ class SSLBio(BIO):
             conn.set_ssl_close_flag(m2.bio_close)
 
     def do_handshake(self):
+        # type: () -> int
         """
         Do the handshake.
 
--- tests/test_bio_file.py
+++ tests/test_bio_file.py
@@ -4,14 +4,15 @@
 
 Copyright (c) 1999-2002 Ng Pheng Siong. All rights reserved."""
 
-import os, sys
+import os
+import sys
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
 
-import M2Crypto
 from M2Crypto.BIO import File, openfile
+
 
 class FileTestCase(unittest.TestCase):
 
@@ -31,22 +32,24 @@ class FileTestCase(unittest.TestCase):
 
     def test_openfile_rb(self):
         # First create the file using Python's open().
-        f = open(self.fname, 'wb')
-        f.write(self.data)
-        f.close()
+        with open(self.fname, 'wb') as f:
+            f.write(self.data)
+
         # Now open the file using M2Crypto.BIO.openfile().
-        f = openfile(self.fname, 'rb')
-        data = f.read(len(self.data))
+        with openfile(self.fname, 'rb') as f:
+            data = f.read(len(self.data))
+
         self.assertEqual(data, self.data)
 
     def test_openfile_wb(self):
         # First create the file using M2Crypto.BIO.openfile().
-        f = openfile(self.fname, 'wb')
-        f.write(self.data)
-        f.close()
+        with openfile(self.fname, 'wb') as f:
+            f.write(self.data)
+
         # Now open the file using Python's open().
-        f = open(self.fname, 'rb')
-        data = f.read(len(self.data))
+        with open(self.fname, 'rb') as f:
+            data = f.read(len(self.data))
+
         self.assertEqual(data, self.data)
 
     def test_closed(self):
@@ -63,8 +66,8 @@ class FileTestCase(unittest.TestCase):
         f2.write(self.data)
         f2.close()
         # Now read the file.
-        f = open(self.fname, 'rb')
-        data = f.read(len(self.data))
+        with open(self.fname, 'rb') as f:
+            data = f.read(len(self.data))
         self.assertEqual(data, self.data)
 
 
