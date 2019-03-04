bdbacebd8740e28be243f9152a06a2aba05c4f7a
Matěj Cepl <mcepl@cepl.eu>
2019-03-04 18:41:39 +0100

    Release 0.32.0

--- M2Crypto/__init__.py
+++ M2Crypto/__init__.py
@@ -20,7 +20,7 @@ Copyright 2008-2011 Heikki Toivonen. All rights reserved.
 # noqa
 import sys
 from distutils.version import StrictVersion
-__version__ = '0.31.0'
+__version__ = '0.32.0'
 version = __version__  # type: str
 version_info = StrictVersion(__version__).version
 
