22f1a82c9f5c058df132db170c334ea01d38241c
Matěj Cepl <mcepl@cepl.eu>
2020-12-08 19:58:56 +0100

    We DO NOT support Python 2.6.

--- setup.py
+++ setup.py
@@ -344,7 +344,6 @@ setuptools.setup(
         'Topic :: Security :: Cryptography',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
