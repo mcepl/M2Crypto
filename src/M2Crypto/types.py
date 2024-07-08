a48a36807e513c12c9f0ba19532603aec630ce5d
Matěj Cepl <mcepl@cepl.eu>
2024-07-09 00:01:13 +0200

    types: minimal infrastructure for type hints for a C extension
    Basics of src/M2Crypto/m2.pyi with types.py with some C types.

---
+++ src/M2Crypto/types.py
@@ -0,0 +1,11 @@
+from typing import NewType
+
+BIO = NewType('BIO', bytes)
+BIO_METHOD = NewType('BIO_METHOD', bytes)
+ASN1_Object = NewType('ASN1_Object', bytes)
+ASN1_String = NewType('ASN1_String', bytes)
+ASN1_Integer = NewType('ASN1_Integer', int)
+ASN1_Time = NewType('ASN1_Time', bytes)
+Engine = NewType('Engine', bytes)
+X509 = NewType('X509', bytes)
+X509_CRL = NewType('X509_CRL', bytes)
